09e4de5ddcdd578b75089a566c551738121dda3d
MurAd <muradisayevazer@gmail.com>
2023-02-27 15:18:10 +0400

    template added for home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,28 @@ import './navbar-cont.css';
 
 function Home() {
   return (
-    <div className='navbar-container'>
+    <div className="navbar-container">
       <NavBar />
-      <p>This is Home page</p>
+      <div className="content-container">
+        <h2>Filankəs</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In
+          asperiores
+        </p>
+        <div className="center-content">
+          <img src="avatar-portf.png" alt="portfolio avatar" />
+          <div className="socials-container">
+            <div className="logo"></div>
+            <div className="logo"></div>
+            <div className="logo"></div>
+          </div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aut
+          itaque asperiores vitae deleniti praesentium odio perferendis, fuga
+          sapiente quidem sed delectus facere minima repudiandae?
+        </p>
+      </div>
     </div>
   );
 }
