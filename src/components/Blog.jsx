06447369e2f88e08e11d15ddfb4a70978cf8ee8e
MurAd <muradisayevazer@gmail.com>
2023-03-01 01:30:05 +0400

    few changes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,11 +1,26 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../App';
 import NavBar from './NavBar';
 import './navbar-cont.css';
+import './Blog.css';
+import BlogContent from './BlogContent';
 
 function Blog() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className='navbar-container'>
+    <div className="navbar-container">
       <NavBar />
-      <p>This is Blog page</p>
+      <div
+        className="blogs-container"
+        id={theme}
+        style={{ backgroundColor: 'transparent' }}
+      >
+        <h2>Blog</h2>
+        <BlogContent />
+        <BlogContent />
+        <BlogContent />
+      </div>
     </div>
   );
 }
---
+++ src/components/BlogContent.jsx
@@ -0,0 +1,16 @@
+import './Blog.css';
+
+function BlogContent() {
+  return (
+    <div className="blog-container">
+      <a href="#">
+        <p className="blog-header">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit
+        </p>
+        <p className="view-counter">999 views</p>
+      </a>
+    </div>
+  );
+}
+
+export default BlogContent;
