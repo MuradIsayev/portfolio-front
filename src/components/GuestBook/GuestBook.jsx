f9ae5b6e53361dd0e4dc3e8f4772d60df7cea88a
MurAd <muradisayevazer@gmail.com>
2023-03-03 10:19:27 +0400

    GuestBook Component's structure was created

--- src/components/GuestBook/GuestBook.jsx
+++ src/components/GuestBook/GuestBook.jsx
@@ -1,11 +1,26 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../../App';
 import NavBar from '../NavBar/NavBar';
 import '../NavBar/navbar-cont.css';
+import './GuestBook.css';
 
 function GuestBook() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className='navbar-container'>
-      <NavBar />
-      <p>This is GuestBook page</p>
+    <div className="main-container">
+      <div className="navbar-container guestbook-navbar-class-block">
+        <NavBar />
+      </div>
+      <div className="guestbook-container" id={theme}
+      style={{backgroundColor:'transparent'}}>
+        <h2>GuestBook</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aut
+          itaque asperiores vitae deleniti praesentium odio perferendis, fuga
+          sapiente quidem sed delectus facere minima repudiandae?
+        </p>
+      </div>
     </div>
   );
 }
