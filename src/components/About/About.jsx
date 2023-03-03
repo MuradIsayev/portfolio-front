86c27fe6f9c6f7603c1475e04ed25b3574d0f72f
MurAd <muradisayevazer@gmail.com>
2023-03-03 10:18:26 +0400

    About Component's structure was created

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,26 @@
 import '../NavBar/navbar-cont.css';
 import NavBar from '../NavBar/NavBar';
+import './About.css';
+import { useContext } from 'react';
+import { ThemeContext } from '../../App';
 
 function About() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className='navbar-container'>
-      <NavBar />
-      <p>This is About page</p>
+    <div className="main-container">
+      <div className="navbar-container about-navbar-class-block">
+        <NavBar />
+      </div>
+      <div className="about-container" id={theme}
+      style={{backgroundColor:'transparent'}}>
+        <h2>About</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aut
+          itaque asperiores vitae deleniti praesentium odio perferendis, fuga
+          sapiente quidem sed delectus facere minima repudiandae?
+        </p>
+      </div>
     </div>
   );
 }
