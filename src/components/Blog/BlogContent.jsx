63b94bce4229c360fbe2435fc455af320007ab08
MurAd <muradisayevazer@gmail.com>
2023-03-06 09:24:12 +0400

    blog date added to the blog component

--- src/components/Blog/BlogContent.jsx
+++ src/components/Blog/BlogContent.jsx
@@ -7,10 +7,13 @@ function BlogContent() {
 
   return (
     <div className="blog-container" id={theme}>
-      <a href="#" className='blog-links'>
-        <p className="blog-header">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit
-        </p>
+      <a href="#" className="blog-links">
+        <div className="blog-fist-content">
+          <p className="blog-header-date">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit
+          </p>
+          <p className="blog-date">NOV 18, 2023</p>
+        </div>
         <p className="view-counter">999 views</p>
       </a>
     </div>
