6fe022093f7b6b0ca9429bbb0ff99d9eeeebee62
MurAd <muradisayevazer@gmail.com>
2023-03-17 15:54:10 +0400

    GitHub auth initialized

--- src/components/GuestBook/GuestBook.jsx
+++ src/components/GuestBook/GuestBook.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ThemeContext } from '../../App';
 import NavBar from '../NavBar/NavBar';
 import GuestBookContent from './GuestBookContent';
@@ -7,9 +7,27 @@ import GuestBookWithLogin from './GuestbookWithLogin';
 import signout from '../../assets/sign-out.svg';
 import '../NavBar/navbar-cont.css';
 import './GuestBook.css';
+import GithubLogin from 'react-github-login';
 
 function GuestBook() {
   const { theme } = useContext(ThemeContext);
+  const [isLogin, setIsLogin] = useState(false);
+  const handleGithubLoginFailure = err => {
+    console.log('Here is the error:', err);
+  };
+
+  const handleGithubLogin = async response => {
+    const { code } = response;
+    const res = await fetch(
+      `http://localhost:3001/api/auth/github/callback?code=${code}`
+    );
+    if (res.ok) {
+      setIsLogin(true);
+      window.location.reload();
+    } else {
+      handleGithubLoginFailure(res.statusText);
+    }
+  };
 
   return (
     <div className="main-container">
@@ -22,8 +40,16 @@ function GuestBook() {
         style={{ backgroundColor: 'transparent' }}
       >
         <h2>GuestBook</h2>
+        <GithubLogin
+          clientId="fb65cc1848303972e11a"
+          onSuccess={handleGithubLogin}
+          onFailure={handleGithubLoginFailure}
+          redirectUri="http://localhost:3001/api/auth/github/callback"
+          scope="user:email"
+          buttonText="Login with GitHub"
+        />
         <div className="guestbook-content-container">
-          <GuestBookWithoutLogin />
+          {isLogin ? <GuestBookWithLogin /> : <GuestBookWithoutLogin />}
           <GuestBookContent />
           <GuestBookContent />
         </div>
