0028ef3b51509f5580447653c661c983b3f92700
MurAd <muradisayevazer@gmail.com>
2023-04-08 18:27:31 +0400

    dark-mode added to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,21 @@ import About from './components/About/About';
 import Blog from './components/Blog/Blog';
 import GuestBook from './components/GuestBook/GuestBook';
 import { Routes, Route } from 'react-router-dom';
-import React, { createContext, useEffect, useState } from 'react';
 import './App.css';
-import ReactSwitch from 'react-switch';
 import Resume from './components/Resume/Resume';
-import { FaMoon, FaSun } from 'react-icons/fa';
-import useDarkMode from './hooks/useDarkMode';
-
-const ThemeIcon = () => {
-  const [darkTheme, setDarkTheme] = useDarkMode();
-  const handleMode = () => setDarkTheme(!darkTheme);
-  return (
-    <span onClick={handleMode}>
-      {darkTheme ? (
-        <FaSun size="24" className="top-navigation-icon" />
-      ) : (
-        <FaMoon size="24" className="top-navigation-icon" />
-      )}
-    </span>
-  );
-};
 
 function App() {
   return (
     <>
-        <div className="App">
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/guestbook" element={<GuestBook />} />
-            <Route path="/resume" element={<Resume />} />
-          </Routes>
-          <ThemeIcon />
-        </div>
+      <div>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/guestbook" element={<GuestBook />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
+import useDarkMode from '../../hooks/useDarkMode';
+import { FaMoon, FaSun } from 'react-icons/fa';
 
 function NavBar() {
   const location = useLocation();
@@ -9,44 +11,58 @@ function NavBar() {
   const isHomePage = location.pathname.includes('/home');
   const isResumePage = location.pathname.includes('/resume');
 
+  const ThemeIcon = () => {
+    const [darkTheme, setDarkTheme] = useDarkMode();
+    const handleMode = () => setDarkTheme(!darkTheme);
+    return (
+      <span onClick={handleMode} className='cursor-pointer'>
+        {darkTheme ? (
+          <FaSun size="24" className="text-yellow-500" />
+        ) : (
+          <FaMoon size="24" className="text-gray-500" />
+        )}
+      </span>
+    );
+  };
+
   return (
-    <div className="container">
-      <nav
-        className="navbar"
-        style={{ backgroundColor: 'transparent' }}
+    <nav
+      className="navbar"
+      style={{ backgroundColor: 'transparent' }}
+    >
+      <ThemeIcon />
+
+      <Link
+        to="/home"
+        className={isHomePage ? 'active navbar-links' : 'navbar-links'}
       >
-        <Link
-          to="/home"
-          className={isHomePage ? 'active navbar-links' : 'navbar-links'}
-        >
-          <span className="navbar-texts">Home</span>
-        </Link>
-        <Link
-          to="/about"
-          className={isAboutPage ? 'active navbar-links' : 'navbar-links'}
-        >
-          <span className="navbar-texts">About</span>
-        </Link>
-        <Link
-          to="/blog"
-          className={isBlogPage ? 'active navbar-links' : 'navbar-links'}
-        >
-          <span className="navbar-texts">Blog</span>
-        </Link>
-        <Link
-          to="/guestbook"
-          className={isGuestBookPage ? 'active navbar-links' : 'navbar-links'}
-        >
-          <span className="navbar-texts">GuestBook</span>
-        </Link>
-        <Link
-          to="/resume"
-          className={isResumePage ? 'active navbar-links' : 'navbar-links'}
-        >
-          <span className="navbar-texts">Resume</span>
-        </Link>
-      </nav>
-    </div>
+        <span className="navbar-texts">Home</span>
+      </Link>
+      <Link
+        to="/about"
+        className={isAboutPage ? 'active navbar-links' : 'navbar-links'}
+      >
+        <span className="navbar-texts">About</span>
+      </Link>
+      <Link
+        to="/blog"
+        className={isBlogPage ? 'active navbar-links' : 'navbar-links'}
+      >
+        <span className="navbar-texts">Blog</span>
+      </Link>
+      <Link
+        to="/guestbook"
+        className={isGuestBookPage ? 'active navbar-links' : 'navbar-links'}
+      >
+        <span className="navbar-texts">GuestBook</span>
+      </Link>
+      <Link
+        to="/resume"
+        className={isResumePage ? 'active navbar-links' : 'navbar-links'}
+      >
+        <span className="navbar-texts">Resume</span>
+      </Link>
+    </nav>
   );
 }
 
