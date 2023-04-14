e160f33a0f9d65136d69adfdad163a27f80194f7
MurAd <muradisayevazer@gmail.com>
2023-04-14 21:23:01 +0400

    motion framer added to the home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,29 +7,50 @@ import darklinkedin from '../../assets/darklinkedin.svg';
 import { motion } from 'framer-motion';
 
 function Home() {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5,
+        delayChildren: .9,
+      },
+    },
+  }
+
+  const items = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1, transition: {
+        type: "tween",
+        staggerChildren: 0.2,
+        delayChildren: .7,
+        duration: ".8",
+      },
+    },
+  }
+
   return (
     <div className="main-container">
       <div className="navbar-container home-navbar-class-block">
         <NavBar />
       </div>
-      <div
+      <motion.div variants={container}
+        initial="hidden"
+        animate="show"
         className="mt-32 w-[90%] md:w-[auto] md:mt-20">
-        <motion.h2 initial={{ x: -1000 }}
-          animate={{ x: 0 }}
-          transition={{
-            type: "tween",
-            duration: ".9",
-            delay: ".5"
-          }} className='headers'>Filankəs</motion.h2>
-        <p className="home-texts">
+        <motion.h2 variants={items}
+          className='headers'>Filankəs</motion.h2>
+        <motion.p variants={items}
+          className="home-texts">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. In
           asperiores
-        </p>
+        </motion.p>
         <div className="flex flex-row items-center justify-start gap-7 mx-auto my-6 md:flex-col md:items-start md:justify-start md:gap-4 md:mx-auto md:my-4">
-          <img className="avatar-img"
+          <motion.img variants={items} className="avatar-img"
             src={avatar} alt="portfolio avatar" />
           <div className="flex flex-col gap-3 justify-center md:items-start md:gap-2">
-            <a className="home-links" href="https://github.com/MuradIsayev">
+            <motion.a variants={items} className="home-links" href="https://github.com/MuradIsayev">
               <motion.img
                 initial={{ scale: 0 }}
                 animate={{ rotate: 360, scale: 1 }}
@@ -49,8 +70,8 @@ function Home() {
                   stiffness: 500,
                   damping: 15
                 }}>My Github</motion.p>
-            </a>
-            <a
+            </motion.a>
+            <motion.a variants={items}
               className="home-links"
               href="https://www.linkedin.com/in/murad-isayev-7735671b5/"
             >
@@ -73,15 +94,15 @@ function Home() {
                   stiffness: 500,
                   damping: 15
                 }}>My LinkedIn</motion.p>
-            </a>
+            </motion.a>
           </div>
         </div>
-        <p className="home-texts">
+        <motion.p variants={items} className="home-texts">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aut
           itaque asperiores vitae deleniti praesentium odio perferendis, fuga
           sapiente quidem sed delectus facere minima repudiandae?
-        </p>
-      </div>
+        </motion.p>
+      </motion.div>
     </div>
   );
 }
