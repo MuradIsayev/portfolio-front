48e8587a7ee0d12346935f01e87a13fbaf494271
MurAd <muradisayevazer@gmail.com>
2023-04-15 22:11:16 +0400

    unnecessary animations removed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -47,55 +47,29 @@ function Home() {
           asperiores
         </motion.p>
         <div className="flex flex-row items-center justify-start gap-7 mx-auto my-6 md:flex-col md:items-start md:justify-start md:gap-4 md:mx-auto md:my-4">
-          <motion.img variants={items} className="avatar-img"
-            src={avatar} alt="portfolio avatar" />
-          <div className="flex flex-col gap-3 justify-center md:items-start md:gap-2">
-            <motion.a variants={items} className="home-links" href="https://github.com/MuradIsayev">
-              <motion.img
-                initial={{ scale: 0 }}
-                animate={{ rotate: 360, scale: 1 }}
-                transition={{
-                  type: "spring",
-                  stiffness: 200,
-                  damping: 20
-                }}
+          <motion.div variants={items}><img  className="avatar-img"
+            src={avatar} alt="portfolio avatar" /></motion.div>
+          <motion.div variants={items} className="flex flex-col gap-3 justify-center md:items-start md:gap-2">
+            <a  className="home-links" href="https://github.com/MuradIsayev">
+              <img
                 className="h-[27px] md:h-[21px]"
                 src={bluegithub}
                 alt="github logo"
               />
-              <motion.p className="md:text-[.65rem]" initial={{ scale: 0 }}
-                animate={{ scale: 1, }}
-                transition={{
-                  type: "spring",
-                  stiffness: 500,
-                  damping: 15
-                }}>My Github</motion.p>
-            </motion.a>
-            <motion.a variants={items}
+              <p className="md:text-[.65rem]">My Github</p>
+            </a>
+            <a 
               className="home-links"
               href="https://www.linkedin.com/in/murad-isayev-7735671b5/"
             >
-              <motion.img
-                initial={{ scale: 0 }}
-                animate={{ rotate: 360, scale: 1 }}
-                transition={{
-                  type: "spring",
-                  stiffness: 200,
-                  damping: 20
-                }}
+              <img
                 className="h-[27px] md:h-[21px]"
                 src={bluelinkedin}
                 alt="linkedin logo"
               />
-              <motion.p className="md:text-[.65rem]" initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                transition={{
-                  type: "spring",
-                  stiffness: 500,
-                  damping: 15
-                }}>My LinkedIn</motion.p>
-            </motion.a>
-          </div>
+              <p className="md:text-[.65rem]">My LinkedIn</p>
+            </a>
+          </motion.div>
         </div>
         <motion.p variants={items} className="home-texts">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aut
