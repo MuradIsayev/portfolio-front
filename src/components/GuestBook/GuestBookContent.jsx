a618d0734ee6552a5621ba9bb3110df88cb4a8f1
MurAd <muradisayevazer@gmail.com>
2023-04-16 15:28:11 +0400

    more changes

--- src/components/GuestBook/GuestBookContent.jsx
+++ src/components/GuestBook/GuestBookContent.jsx
@@ -5,7 +5,7 @@ function GuestBookContent() {
       <div className="flex flex-row justify-start gap-[4px] 
                       mt-2 w-[90%] font-[300] text-[.96rem]
                 md:mt-1 md:w-[90%] md:font-[400] md:text-[.77rem]">
-        <span className="font-[500]">User: </span>
+        <span className="font-[400]">User: </span>
         <span>text</span>
       </div>
     </div>
