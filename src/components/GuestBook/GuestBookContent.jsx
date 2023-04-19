be8df8ab6dcd447a4cdff43234c4809f3be66df0
MurAd <muradisayevazer@gmail.com>
2023-04-20 00:00:35 +0400

    motion framer removed

--- src/components/GuestBook/GuestBookContent.jsx
+++ src/components/GuestBook/GuestBookContent.jsx
@@ -1,5 +1,3 @@
-import { motion } from "framer-motion";
-
 const GuestBookContent = ({ data }) => {
   return (
     <div>
@@ -9,8 +7,8 @@ const GuestBookContent = ({ data }) => {
             <div className="flex flex-row justify-start items-center gap-[3px] 
                       mt-2 max-w-[60%] font-[300] text-[.96rem] 
                 md:mt-1 md:w-[90%] md:font-[400] md:text-[.77rem]">
-              <motion.img
-                src={userData?.photoURL} alt="Profile photo" className="rounded-full w-8 border border-gray-300 mr-1 transition ease-in-out duration-100 blur-[.7px] hover:blur-0" />
+              <img
+                src={userData?.photoURL} alt="Profile photo" className="rounded-full w-7 border border-gray-300 mr-1 transition ease-in-out duration-100 blur-[.8px] hover:blur-0" />
               <span className="font-[400]">{userData?.userName ? userData?.userName : 'Anonymous'}:</span>
               <span className="max-w-[85%]">{message}</span>
             </div>
