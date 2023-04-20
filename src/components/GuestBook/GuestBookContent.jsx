01a922477b19a1329301aee627b58d33b42e8d72
MurAd <muradisayevazer@gmail.com>
2023-04-20 19:46:18 +0400

    .

--- src/components/GuestBook/GuestBookContent.jsx
+++ src/components/GuestBook/GuestBookContent.jsx
@@ -1,6 +1,4 @@
-// TODO: Fix space between navbar and guestbook (when messages span is removed, it works fine)
-// TODO 2: Fix text overflow
-// TODO 3: Fix dark mode for guestbook
+// TODO: Fix text overflow
 
 const GuestBookContent = ({ data }) => {
   const allMessages = [];
@@ -22,10 +20,10 @@ const GuestBookContent = ({ data }) => {
       {allMessages?.reverse().map((message) => (
         <div key={`${message.userName}-${message.createdAt}`} className="mt-2">
           <div className="flex flex-row justify-start gap-[5px] 
-                      mt-4 font-[300] text-[.95rem]
+                      mt-5 mb-5 font-[300] text-[.95rem]
                 md:mt-3 md:text-[.63rem]">
             <img
-              src={message?.photoURL} alt="Profile photo" className="mb-auto md:-mt-[.2rem] -mt-[.1rem] rounded-full w-7 md:w-6 border border-gray-600 dark:border-gray-200 mr-1" />
+              src={message?.photoURL} alt="Profile photo" className="mb-auto md:-mt-[.2rem] -mt-[.2rem] rounded-full w-[30px] md:w-6 border border-gray-600 dark:border-gray-200 mr-1" />
             <span className="font-[400]">{message?.userName ? message?.userName : 'Anonymous'}:</span>
             <span className="w-[70%] md:w-[70%] break-words ">{message?.message}</span>
           </div>
