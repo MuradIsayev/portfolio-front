54bb298a2c6df1d56cec4249569d93f8071f0c4e
MurAd <muradisayevazer@gmail.com>
2023-04-20 15:09:01 +0400

    small bugs fixed

--- src/components/GuestBook/GuestBookContent.jsx
+++ src/components/GuestBook/GuestBookContent.jsx
@@ -1,3 +1,5 @@
+// TODO: Fix text length
+
 const GuestBookContent = ({ data }) => {
   const allMessages = [];
   data.forEach((userData) => {
@@ -18,12 +20,12 @@ const GuestBookContent = ({ data }) => {
       {allMessages?.reverse().map((message) => (
         <div className="mt-2">
           <div className="flex flex-row justify-start gap-[5px] 
-                      mt-2 max-w-[60%] font-[300] text-[.96rem] 
-                md:mt-1 md:font-[400] md:text-[.77rem]">
+                      mt-4 font-[300] text-[.95rem]
+                md:mt-3 md:text-[.63rem]">
             <img
-              src={message?.photoURL} alt="Profile photo" className="mb-auto rounded-full w-7 border border-gray-300 mr-1 transition ease-in-out duration-100 blur-[.8px] hover:blur-0" />
+              src={message?.photoURL} alt="Profile photo" className="mb-auto md:-mt-[.2rem] -mt-[.1rem] rounded-full w-7 md:w-6 border border-gray-300 mr-1 transition ease-in-out duration-100 blur-[.8px] hover:blur-0" />
             <span className="font-[400]">{message?.userName ? message?.userName : 'Anonymous'}:</span>
-            <span className="max-w-[85%] break-words">{message?.message}</span>
+            <span className="w-[70%] md:w-[70%] break-words ">{message?.message}</span>
           </div>
         </div>
       ))}
