d1070557b7ed04fe713f1f082b0d5b7f0d444f29
MurAd <muradisayevazer@gmail.com>
2023-04-20 13:37:50 +0400

    guest image flex and message length fixed

--- src/components/GuestBook/GuestBookContent.jsx
+++ src/components/GuestBook/GuestBookContent.jsx
@@ -17,13 +17,13 @@ const GuestBookContent = ({ data }) => {
     <div>
       {allMessages?.reverse().map((message) => (
         <div className="mt-2">
-          <div className="flex flex-row justify-start items-center gap-[5px] 
+          <div className="flex flex-row justify-start gap-[5px] 
                       mt-2 max-w-[60%] font-[300] text-[.96rem] 
-                md:mt-1 md:w-[90%] md:font-[400] md:text-[.77rem]">
+                md:mt-1 md:font-[400] md:text-[.77rem]">
             <img
-              src={message?.photoURL} alt="Profile photo" className="rounded-full w-7 border border-gray-300 mr-1 transition ease-in-out duration-100 blur-[.8px] hover:blur-0" />
+              src={message?.photoURL} alt="Profile photo" className="mb-auto rounded-full w-7 border border-gray-300 mr-1 transition ease-in-out duration-100 blur-[.8px] hover:blur-0" />
             <span className="font-[400]">{message?.userName ? message?.userName : 'Anonymous'}:</span>
-            <span className="max-w-[85%]">{message?.message}</span>
+            <span className="max-w-[85%] break-words">{message?.message}</span>
           </div>
         </div>
       ))}
