5c91dd639414a1b0c8dd5a509a0dc01aafb2d3a6
MurAd <muradisayevazer@gmail.com>
2023-04-27 22:47:18 +0400

    updates on avatars and length of text

--- src/components/GuestBook/GuestBookContent.jsx
+++ src/components/GuestBook/GuestBookContent.jsx
@@ -21,23 +21,24 @@ const GuestBookContent = ({ data }) => {
     <div>
       {allMessages?.reverse().map((message) => (
         <div key={`${message.userName}-${message.createdAt}`} className="mt-2">
-          <div className="flex flex-row justify-start gap-[4px] whitespace-nowrap
+          <div className="flex flex-row justify-start gap-[4px]
                       mt-5 mb-5 md:mt-4 md:mb-4 font-[300] text-[.95rem] md:text-[.63rem]">
-            <div className="avatar w-[35px] h-[35px] mb-auto md:w-[23px] md:h-[23px] mr-[7px] md:mr-[2px]">
+            <div className="avatar w-[35px] h-[35px] mb-auto md:w-[23px] md:h-[23px] mr-[7px] md:mr-[3px]">
               <img
                 src={message?.photoURL} alt="Profile photo" className="outline rounded-full outline-[2px] md:outline-[1px]
-                                                                       outline-[#67b3d1] md:-mt-[.2rem] -mt-[.4rem]" />
+                                                                       outline-[#61a6c2] md:-mt-[.25rem] -mt-[.4rem]  " />
               {message?.isOnline ? <span className="-top-[7px] left-[26.3px] absolute w-3 h-3 
                         bg-green-400 border-2  dark:border-black border-white rounded-full
-                          md:-top-[2.5px] md:left-[17.7px]  md:w-[7px] md:h-[7px] md:border-[1px]">
+                          md:-top-[3.6px] md:left-[17.7px]  md:w-[7px] md:h-[7px] md:border-[1px]">
               </span> : <span className="-top-[7px] left-[26.3px] absolute w-3 h-3 
                         bg-gray-500 border-2  dark:border-black border-white rounded-full
-                          md:-top-[2.5px] md:left-[17.7px] md:w-[7px] md:h-[7px] md:border-[1px]">
+                          md:-top-[3.6px] md:left-[17.7px] md:w-[7px] md:h-[7px] md:border-[1px]">
               </span>}
-
             </div>
-            <span className="font-[400]">{message?.userName}:</span>
-            <span className="w-[70%] md:w-[70%] break-words ">{message?.message}</span>
+            <div className="flex w-[85%] gap-[.2rem]">
+              <span className=" max-w-[20%] md:max-w-[35%] font-[400] md:whitespace-nowrap">{message?.userName}:</span>
+              <span className=" w-[85%] md:w-[75%] break-words">{message?.message}</span>
+            </div>
           </div>
         </div>
       ))}
