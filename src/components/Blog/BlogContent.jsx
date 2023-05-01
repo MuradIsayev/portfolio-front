e7c9adda4cd7380cc3ed2f2362801f855bb6bae0
MurAd <muradisayevazer@gmail.com>
2023-05-01 13:28:40 +0400

    new blog container established

--- src/components/Blog/BlogContent.jsx
+++ src/components/Blog/BlogContent.jsx
@@ -2,16 +2,11 @@ import './Blog.css';
 
 const BlogContent = () => {
   return (
-    <div className="blog-container">
-      <a href="#" className="blog-links">
-        <div className="blog-fist-content">
-          <p className="blog-header-date">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit
-          </p>
-          <p className="blog-date">NOV 18, 2023</p>
+    <div className="h-28 flex flex-col flex-wrap bg-gray-200 w-[80%] md:w-[97%] rounded-md mb-4 dark:bg-[#292929d2]">
+      <span className='absolute w-[6px] h-28 bg-blue-400 rounded-l-md'></span>
+      <div className='flex flex-row justify-between items-center w-[100%]'>
+        <h3 className='text-[.9rem] md:text-[.8rem] font-bold ml-2'>Blog Title</h3>
         </div>
-        <p className="view-counter">999 views</p>
-      </a>
     </div>
   );
 }
