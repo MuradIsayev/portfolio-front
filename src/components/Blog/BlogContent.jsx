de714cdba7c07cf346885884c0ee1743b574fd7f
MurAd <muradisayevazer@gmail.com>
2023-05-04 19:01:25 +0400

    blog content component modified

--- src/components/Blog/BlogContent.jsx
+++ src/components/Blog/BlogContent.jsx
@@ -2,19 +2,26 @@ import './Blog.css';
 
 const BlogContent = () => {
   return (
-    <div className="h-[7.7rem] md:h-24 flex flex-col flex-wrap cursor-pointer transition duration-150 
-                    ease-linear hover:outline hover:outline-[2px] hover:outline-[#8ab2c5] w-[80%] 
-                    md:w-[97%] rounded-md mb-3 md:mb-2">
+    <div className="h-[7.7rem]  md:h-24 flex flex-col flex-wrap justify-center cursor-pointer transition duration-150 
+                    ease-linear hover:outline hover:outline-[2px] hover:outline-[#8ab2c5] dark:hover:outline-[#2c4a58] w-[80%] 
+                    md:w-[97%] rounded-md mb-7 md:mb-5 mt-1.5">
       {/* <span
         className='absolute w-[5px] md:w-[4px] h-[6.8rem] md:h-24 bg-blue-300 rounded-l-md'
       ></span> */}
-      <div className='flex flex-row flex-wrap justify-center items-center max-w-[100%] md:max-w-[95%] mt-1 ml-2 md:ml-1'>
-        <div className='flex flex-col justify-center gap-1 md:gap-0 items-center mb-3 md:mb-1'>
+      <div className='flex flex-row flex-wrap justify-center items-center md:justify-start max-w-[100%] md:max-w-[95%] ml-2 md:ml-1'>
+        <div className='flex flex-col justify-center items-center md:items-start md:mb-3 mb-4'>
           <h3 className='text-[1.1rem] md:text-[.79rem] font-medium'>Blog Title</h3>
-          <div className='text-[.75rem] md:text-[.53rem] text-gray-400'>June 8, 2023 | 1min read.</div>
+          <div className='flex gap-1 md:gap-[3px] text-[.75rem] md:text-[.53rem] text-gray-400 dark:text-[#a7a4a4]'>
+            <span className='dark:hover:text-[#fff] hover:text-[#000] duration-100 
+                    ease-linear'>June 8, 2023</span>
+            <span>|</span>
+            <span className='dark:hover:text-[#fff] hover:text-[#000] duration-100 
+                    ease-linear'>3 min read</span>
+          </div>
         </div>
         <div className='text-[.91rem] md:text-[.6rem] '>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem ipsam repellendus a accusamus asperiores expedita soluta praesentium? Odit, reprehenderit. Quisquam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem ipsam repellendus a 
+          accusamus asperiores expedita soluta praesentium? Odit, reprehenderit. Quisquam.
         </div>
       </div>
     </div>
