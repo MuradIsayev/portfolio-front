8c844d5e9f05801ea845ee73537c6a5f40b71904
MurAd <muradisayevazer@gmail.com>
2023-05-04 22:17:56 +0400

    dynamic data added for blog content and few updates

--- src/components/Blog/BlogContent.jsx
+++ src/components/Blog/BlogContent.jsx
@@ -1,6 +1,6 @@
 import './Blog.css';
 
-const BlogContent = () => {
+const BlogContent = ({ title, minsRead, createdAt }) => {
   return (
     <div className="h-[7.7rem]  md:h-24 flex flex-col flex-wrap justify-center cursor-pointer transition duration-150 
                     ease-linear hover:outline hover:outline-[2px] hover:outline-[#8ab2c5] dark:hover:outline-[#2c4a58] w-[80%] 
@@ -10,13 +10,13 @@ const BlogContent = () => {
       ></span> */}
       <div className='flex flex-row flex-wrap justify-center items-center md:justify-start max-w-[100%] md:max-w-[95%] ml-2 md:ml-1'>
         <div className='flex flex-col justify-center items-center md:items-start md:mb-3 mb-4'>
-          <h3 className='text-[1.1rem] md:text-[.79rem] font-medium'>Blog Title</h3>
+          <h3 className='text-[1.1rem] md:text-[.79rem] font-medium'>{title}</h3>
           <div className='flex gap-1 md:gap-[3px] text-[.75rem] md:text-[.53rem] text-gray-400 dark:text-[#a7a4a4]'>
             <span className='dark:hover:text-[#fff] hover:text-[#000] duration-100 
-                    ease-linear'>June 8, 2023</span>
+                    ease-linear'>{createdAt}</span>
             <span>|</span>
             <span className='dark:hover:text-[#fff] hover:text-[#000] duration-100 
-                    ease-linear'>3 min read</span>
+                    ease-linear'>{minsRead} min read</span>
           </div>
         </div>
         <div className='text-[.91rem] md:text-[.6rem] '>
