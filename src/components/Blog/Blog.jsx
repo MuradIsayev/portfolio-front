06e6a800755db361f2e5717ab0e4e8c41a648ebe
MurAd <muradisayevazer@gmail.com>
2023-05-07 02:23:29 +0400

    blog component few fixes

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,29 +1,28 @@
 import { useEffect, useState } from 'react';
 import NavBar from '../NavBar/NavBar';
-import './Blog.css';
 import BlogContent from './BlogContent';
 import { Client } from '@notionhq/client';
 import axios from 'axios';
+import BlogDetails from './BlogDetails';
 
 const client = new Client({
   auth: import.meta.env.VITE_APP_NOTION_KEY,
 });
 
 const Blog = () => {
-  const [data, setData] = useState([]);
   const [blogData, setBlogData] = useState([]);
-  useEffect(() => {
-    axios.get('http://localhost:3001/api/blogs/f5349c902cdf45deafe570f1f9bc1fd4').then(response => {
-      setData(response?.data);
-      console.log(response?.data);
-    });
-  }, []);
+  const [selectedBlogId, setSelectedBlogId] = useState(null); // initialize the selected blog id to null
 
   useEffect(() => {
     axios.get('http://localhost:3001/api/blogs').then(response => {
       setBlogData(response?.data);
     });
   }, []);
+
+  const handleBlogSelection = (blogId) => {
+    setSelectedBlogId(blogId);
+    console.log(blogId);
+  };
 
   return (
     <div className="main-container">
@@ -37,79 +36,27 @@ const Blog = () => {
           <h2 className='headers'>Blog</h2>
           <span className='mr-2 text-[.92rem] md:text-[.65rem] font-bold'>{blogData.length} articles</span>
         </div>
-        <div>
-          {blogData?.map(({id, title, description, minsRead, createdAt }) => {
-            return (
-              <BlogContent key={id} title={title} description={description} minsRead={minsRead} createdAt={createdAt} />
-            )
-          })}
-        </div>
-        {/* <div>
-          {data?.map((block) => {
-            const { type, [type]: content } = block;
-
-            switch (type) {
-              case "heading_3":
+        {selectedBlogId ? ( // conditionally render the BlogDetails component if a blog is selected
+          <BlogDetails blogId={selectedBlogId} setSelectedBlogId={setSelectedBlogId} />
+        ) : (
+          <>
+            <div>
+              {blogData?.map(({ id, blockId, title, description, minsRead, createdAt }) => {
                 return (
-                  <h3 key={block.id} style={{ color: content.color, marginTop: '2rem' }}>
-                    {content?.text.map((text, index) => (
-                      <span
-                        key={`${block.id}-text-${index}`}
-                        style={{
-                          fontWeight: text.annotations?.bold ? 'bold' : 'normal',
-                          fontStyle: text.annotations?.italic ? 'italic' : 'normal',
-                          textDecoration: text.annotations?.underline ? 'underline' : 'none',
-                          color: text?.annotations?.color,
-                        }}
-                      >
-                        {text.plain_text}
-                      </span>
-                    ))}
-                  </h3>
+                  <BlogContent
+                    key={id}
+                    title={title}
+                    blockId={blockId}
+                    description={description}
+                    minsRead={minsRead}
+                    createdAt={createdAt}
+                    handleClick={handleBlogSelection} // pass the blog id to the handler function
+                  />
                 );
-              case "paragraph":
-                return (
-                  <p key={block.id} style={{ color: content.color }}>
-                    {content?.text.map((text, index) => (
-                      <span
-                        key={`${block.id}-text-${index}`}
-                        style={{
-                          fontWeight: text.annotations?.bold ? 'bold' : 'normal',
-                          fontStyle: text.annotations?.italic ? 'italic' : 'normal',
-                          textDecoration: text.annotations?.underline ? 'underline' : 'none',
-                          color: text?.annotations?.color,
-                        }}
-                      >
-                        {text.plain_text}
-                      </span>
-                    ))}
-                  </p>
-                );
-              case "bulleted_list_item":
-                return (
-                  <ul key={block.id} style={{ color: content.color, listStyleType: 'disc', marginLeft: '20px' }} >
-                    <li>
-                      {content?.text.map((text, index) => (
-                        <span
-                          key={`${block.id}-text-${index}`}
-                          style={{
-                            fontWeight: text.annotations?.bold ? 'bold' : 'normal',
-                            fontStyle: text.annotations?.italic ? 'italic' : 'normal',
-                            textDecoration: text.annotations?.underline ? 'underline' : 'none',
-                            color: text?.annotations?.color,
-                          }}
-                        >
-                          {text.plain_text}
-                        </span>
-                      ))}
-                    </li>
-                  </ul>
-                );
-              default:
-                return null;
-            }
-          })}
-        </div> */}
+              })}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
