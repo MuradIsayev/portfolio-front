2e3cbbb93ac2460b7a014eb989c758faf89af53e
MurAd <muradisayevazer@gmail.com>
2023-05-09 21:01:43 +0400

    tag filtering added

--- src/components/Blog/BlogTags.jsx
+++ src/components/Blog/BlogTags.jsx
@@ -1,9 +1,25 @@
-const BlogTags = ({tag}) => {
+import axios from "axios";
+
+const handleTagClick = async (tag, setBlogData) => {
+    await axios.get(`http://localhost:3001/api/blogs/search?tag=${tag}`)
+        .then(response => {
+            setBlogData(response?.data); // logs the filtered blogs array
+        }).catch(error => {
+            console.log(error);
+        });
+};
+
+const BlogTags = ({ tag, setBlogData }) => {
     return (
         <div>
-            <div className="badge badge-outline border-gray-300 p-3 hover:bg-[#ebebeb]">{tag}</div>
+            <div
+                onClick={() => handleTagClick(tag, setBlogData)}
+                className="badge badge-outline border-gray-300 p-3 hover:bg-[#ebebeb] cursor-pointer"
+            >
+                {tag}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default BlogTags;
