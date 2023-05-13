8ecc0aabb8e675e96b88118ba8eec12a84eff40c
MurAd <muradisayevazer@gmail.com>
2023-05-13 21:02:57 +0400

    small changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
       <div
         className="flex flex-col justify-start w-[90%] mt-[7.3rem] md:mt-20 md:w-auto"
       >
-        <div className='mb-3'>
+        <div className='mb-4'>
           <h2 className='headers'>About</h2>
           <p className='home-texts'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aut
@@ -29,18 +29,18 @@ const About = () => {
           </p>
         </div>
 
-        <ol className="relative border-l border-gray-200 dark:border-gray-700 mt-2">
+        <ol className="relative border-l border-gray-200 dark:border-gray-700">
           <li className="mb-10 ml-4">
             <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
               <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
             </span>
-            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Jan 2022 ‑ Apr 2023</time>
-            <div>
+            <time className="text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Jan 2022 ‑ Apr 2023</time>
+            <div className='flex gap-1 items-end'>
               <span className="text-base font-semibold text-gray-900 dark:text-white">Software Developer</span>
-              <span className="text-[14px] font-medium text-gray-600 dark:text-gray-400">  at</span>
-              <span className="text-[14px] font-medium text-gray-600 dark:text-gray-400"> UFAZ</span>
-              <span className="text-[14px] font-medium text-gray-600 dark:text-gray-400"> ·</span>
-              <span className="text-[14px] font-medium text-gray-600 dark:text-gray-400"> Part-time</span>
+              <span className="text-[.87rem] font-medium text-gray-700 dark:text-gray-400">at</span>
+              <span className="text-[.87rem] font-medium text-gray-700 dark:text-gray-400">UFAZ</span>
+              <span className="text-[.87rem] font-medium text-gray-700 dark:text-gray-400">·</span>
+              <span className="text-[.87rem] font-medium text-gray-700 dark:text-gray-400">Part-time</span>
             </div>
             <p className="mb-4 text-sm font-normal text-gray-500 dark:text-gray-400">Helped developing a full‑stack application for Enterprise Resource Planning and Learning Management System using modern programming languages
               and frameworks, completed all the tasks which were assigned by the project manager during the sprint, and implementing the small and large functionalities of the system</p>
