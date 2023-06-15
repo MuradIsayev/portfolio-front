5672fbeb8eb71653fe829c15d4290d107d8dfa71
MurAd <muradisayevazer@gmail.com>
2023-06-15 23:44:56 +0400

    new card ui added

--- src/components/About/ProjectCard.jsx
+++ src/components/About/ProjectCard.jsx
@@ -1,41 +1,22 @@
 import './About.css';
 import darkgithub from '../../assets/socials/darkgithub.svg';
 import bluegithub from '../../assets/socials/bluegithub.svg';
-import { motion } from 'framer-motion';
 
-const ProjectAbout = ({ name, description, url, skills, testValue }) => {
+const ProjectAbout = ({ name, description, url, skills, color, darkColor }) => {
 
   return (
-    <motion.div
-      className="project-container"
-      initial={{
-        opacity: 0,
-        translateX: testValue % 2 === 0 ? -50 : 50,
-        translateY: -50,
-      }}
-      animate={{ opacity: 1, translateX: 0, translateY: 0 }}
-      transition={{ duration: 0.2, delay: testValue * 0.1 }}
-    >
-      <h3 className="project-header">{name}</h3>
-      <div className="desc-skill-container">
-        <p className="project-text">{description}</p>
-        <ul class="skill-list">
-          {skills.map(skill => (
-            <li className="skill-item">{skill.name}</li>
-          ))}
-        </ul>
+    <div class="w-[44%] column">
+      <div className={`card ${darkColor} ${color}`}>
+        <div class="txt dark:text-white text-black">
+          <h1>{name}</h1>
+          <p>{description}</p>
+        </div>
+        <a href={url} target='_blank'>more</a>
+        <div class="ico-card">
+          <i class="fa fa-rebel"></i>
+        </div>
       </div>
-      <div className="link-container">
-        <a href={url}>
-          <img
-            className="github-img"
-            src={bluegithub}
-            alt="github logo"
-          />
-          <p className="link-text">View Source</p>
-        </a>
-      </div>
-    </motion.div>
+    </div>
   );
 }
 
