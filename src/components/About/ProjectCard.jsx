61b1fbe17cb519a56e7a3d7aa049ebe0e9bdfb54
MurAd <muradisayevazer@gmail.com>
2023-06-17 22:56:41 +0400

    group feature added

--- src/components/About/ProjectCard.jsx
+++ src/components/About/ProjectCard.jsx
@@ -6,9 +6,9 @@ const ProjectAbout = ({ name, description, url, skills, color, darkColor }) => {
 
   return (
     <div class="w-[42.5%] column">
-      <div className={`card ${darkColor} ${color}`}>
+      <div className={`group card ${darkColor} ${color}`}>
         <div class="txt dark:text-white text-black">
-          <h1 className='project-name'>{name}</h1>
+          <h1 className='project-name group-hover:text-white'>{name}</h1>
           <p className='project-desc'>{description}</p>
         </div>
         <a href={url} className='project-link' target='_blank'>more</a>
