b9e7a53ebf37be5f6fb2149ddf3d32fcf2b3395b
MurAd <muradisayevazer@gmail.com>
2023-09-03 18:55:02 +0400

    new import export setup added

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import goBack from './goback.svg';
+import signOut from './signout/signout.svg';
+import github from './socials/bluegithub.svg';
+import google from './socials/google.svg';
+import linkedin from './socials/bluelinkedin.svg';
+import myAvatar from './myavatar.jpg';
+
+export { goBack, signOut, github, google, linkedin, myAvatar };
