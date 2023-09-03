0648ac54a0b7634fb5c2432708804f15449087c3
MurAd <muradisayevazer@gmail.com>
2023-09-03 20:50:55 +0400

    nav links constants added

---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+
+export const navLinks = [
+    {
+        name: 'Home',
+        link: '/home',
+    },
+    {
+        name: 'About',
+        link: '/about',
+    },
+    {
+        name: 'Blogs',
+        link: '/blogs',
+    },
+    {
+        name: 'Guestbook',
+        link: '/guestbook',
+    },
+];
