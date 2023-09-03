67d35da1ec05e3dd9e7b16f0e45dcf2612300154
MurAd <muradisayevazer@gmail.com>
2023-09-03 19:46:31 +0400

    file name and folder structure changes

---
+++ src/components/BlogTags.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import '../styles/brackets.scss'
+
+const handleTagClick = async (tag, setBlogData) => {
+    await axios
+        .get(`${import.meta.env.VITE_APP_BACKEND_URL}/blogs/search?tag=${tag}`)
+        .then((response) => {
+            setBlogData(response?.data); // logs the filtered blogs array
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+const BlogTags = ({ tag, setBlogData }) => {
+    return (
+        <div>
+            <div
+                onClick={() => handleTagClick(tag, setBlogData)}
+                className="brackets text-[#d31a1a] opacity-75 hover:opacity-100 duration-75 ease-linear
+                         dark:text-[#be6060]"
+            >
+                {tag}
+            </div>
+        </div>
+    );
+};
+
+export default BlogTags;
---
+++ src/components/NotLoggedInGuest.jsx
@@ -0,0 +1,35 @@
+import { github } from '../assets';
+import { google } from '../assets';
+
+
+const NotLoggedInGuest = ({ signInWithGitHub, signInWithGoogle }) => {
+
+  return (
+    <div className="flex flex-row items-center gap-5 mb-8 md:gap-3">
+      <div>
+        <button className="login-with-github">
+          <img
+            className="w-[21px] md:w-[14px] md:h-[14px]"
+            src={github}
+            alt="github logo"
+          />
+          <span className="text-[.83rem] font-[500] md:text-[.5rem]" onClick={signInWithGitHub}>Sign in with GitHub</span>
+        </button>
+      </div>
+      <span className="text-[.9rem] font-[400] md:text-[.5rem]">or</span>
+      <div>
+        <button className="login-with-github">
+          <img
+            className="w-[18px] h-[18px] md:w-[12px]  md:h-[12px]"
+            src={google}
+            alt="github logo"
+          />
+          <span className="text-[.83rem] font-[500] md:text-[.5rem]" onClick={signInWithGoogle}>Sign in with Google</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+
+export default NotLoggedInGuest;
