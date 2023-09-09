695c29266df52d0229f43b00b36731276944aca7
MurAd <muradisayevazer@gmail.com>
2023-09-09 17:11:32 +0400

    not found and underConstruction components updated

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
             <div
                 className="mt-[7.3rem] w-[90%] md:w-full md:mt-20"
             >
-                <h2 className="headers">Oops! You seem to be lost.</h2>
+                <h2 className="headers uppercase font-secondary text-[1.3rem]">Oops! You seem to be lost.</h2>
             </div>
         </>
     )
--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
 
 const UnderConstruction = () => {
     return (
-        <div>UnderConstruction</div>
+        <>
+            <div
+                className="mt-[7.3rem] w-[90%] md:w-full md:mt-20"
+            >
+                <h2 className="headers uppercase font-secondary text-[1.3rem]">Shhh, I am still working on it! <br />We didn't see each other.</h2>
+            </div>
+        </>
     )
 }
 
-export default UnderConstruction
+export default UnderConstruction;
