2f71dc384ff8a1ace522668524b12ef90d6e7b23
MurAd <muradisayevazer@gmail.com>
2023-09-11 23:40:36 +0400

    Guestbook content updated

--- src/components/GuestBookContent.jsx
+++ src/components/GuestBookContent.jsx
@@ -14,24 +14,20 @@ const GuestBookContent = ({ data }) => {
   });
 
   allMessages.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-
+ // mb-auto above img container
   return (
     <div>
       {allMessages?.reverse().map((message) => (
         <div key={`${message.userName}-${message.createdAt}`} className="mt-2">
-          <div className="flex flex-row justify-start gap-[4px]
+          <div className="flex flex-row justify-start items-center gap-[4px]
                       mt-5 mb-5 md:mt-4 md:mb-4 font-[300] text-[.95rem] md:text-[.63rem]">
-            <div className="avatar w-[35px] h-[35px] mb-auto md:w-[23px] md:h-[23px] mr-[7px] md:mr-[3px]">
+            <div className="w-10 h-10 rounded-full object-contain md:w-6 md:h-6 mr-[7px] md:mr-[3px] relative">
               <img
-                src={message?.photoURL} alt="Profile photo" className="outline rounded-full outline-[2px] md:outline-[1px]
-                                                                       outline-[#61a6c2] md:-mt-[.25rem] -mt-[.4rem]  " />
-              {message?.isOnline ? <span className="-top-[7px] left-[26.3px] absolute w-3 h-3 
-                        bg-green-400 border-2  dark:border-black border-white rounded-full
-                          md:-top-[3.6px] md:left-[17.7px]  md:w-[7px] md:h-[7px] md:border-[1px]">
-              </span> : <span className="-top-[7px] left-[26.3px] absolute w-3 h-3 
-                        bg-gray-500 border-2  dark:border-black border-white rounded-full
-                          md:-top-[3.6px] md:left-[17.7px] md:w-[7px] md:h-[7px] md:border-[1px]">
-              </span>}
+                src={message?.photoURL} alt="Profile photo" className="rounded-full" />
+              {message?.isOnline ? <span class="top-0 left-7 absolute w-3 h-3 bg-green-400 border-2 border-white dark:border-[#09090B] rounded-full
+                  md:left-4 md:w-[7px] md:h-[7px] md:border-[1px]
+              "></span>
+                : <span class="top-0 left-7 absolute md:left-4 md:w-[7px] md:h-[7px] md:border-[1px]  w-3 h-3 dark:bg-gray-300 bg-gray-400  border-2 border-white dark:border-[#09090B] rounded-full"></span>}
             </div>
             <div className="flex w-[85%] gap-[.2rem]">
               <span className=" max-w-[20%] md:max-w-[35%] font-[400] md:whitespace-nowrap">{message?.userName}:</span>
@@ -39,8 +35,9 @@ const GuestBookContent = ({ data }) => {
             </div>
           </div>
         </div>
-      ))}
-    </div>
+      ))
+      }
+    </div >
   );
 }
 
