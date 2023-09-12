550c94115c10a78ad47bfd314beb64b30b0a1113
MurAd <muradisayevazer@gmail.com>
2023-09-12 22:22:02 +0400

    code simplified and bug fixed on the guestbook content

--- src/components/GuestBookContent.jsx
+++ src/components/GuestBookContent.jsx
@@ -14,24 +14,25 @@ const GuestBookContent = ({ data }) => {
   });
 
   allMessages.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-  // mb-auto above img container
   return (
     <div>
       {allMessages?.reverse().map((message) => (
         <div key={`${message.userName}-${message.createdAt}`} className="mt-2">
-          <div className="flex flex-row justify-start items-center gap-[4px]
-                      mt-5 mb-5 md:mt-4 md:mb-4 ">
-            <div className="w-10 h-10 rounded-full object-contain md:w-6 md:h-6 mr-[7px] md:mr-[3px] relative">
+          <div key={message?.createdAt} className="flex flex-row items-center mb-4">
+
+            <div className="w-10 h-10 rounded-full object-contain md:w-6 md:h-6 mr-[7px] md:mr-[3px] relative mb-auto">
               <img
-                src={message?.photoURL} alt="Profile photo" className="rounded-full" />
+                src={message?.photoURL} alt="Profile photo" className="rounded-full " />
               {message?.isOnline ? <span class="top-0 left-7 absolute w-3 h-3 bg-green-400 border-2 border-white dark:border-[#09090B] rounded-full
                   md:left-4 md:w-[7px] md:h-[7px] md:border-[1px]
               "></span>
                 : <span class="top-0 left-7 absolute md:left-4 md:w-[7px] md:h-[7px] md:border-[1px]  w-3 h-3 dark:bg-gray-300 bg-gray-400  border-2 border-white dark:border-[#09090B] rounded-full"></span>}
             </div>
-            <div className="flex w-[85%] gap-[.2rem]">
-              <span className="text-[.95rem] md:text-[.65rem] max-w-[20%] md:max-w-[35%] font-[400] md:whitespace-nowrap dark:text-[#e7e6e6e8] text-[#09090bdc] ">{message?.userName}:</span>
-              <span className="text-[.95rem] md:text-[.65rem] w-[85%] md:w-[75%] font-[400] break-words text-[#18181B] dark:text-[#ececec]">{message?.message}</span>
+            <div className="text-sm md:text-xs break-words md:w-[90%]">
+              <span className="mr-1 text-neutral-600 whitespace-nowrap dark:text-neutral-400">
+                {message?.userName}:
+              </span>
+              {message?.message}
             </div>
           </div>
         </div>
