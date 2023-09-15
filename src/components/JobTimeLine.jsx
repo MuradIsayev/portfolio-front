921c8704118cb099c8dc45f362602c8839849878
MurAd <muradisayevazer@gmail.com>
2023-09-15 23:36:30 +0400

    css updated in about page

--- src/components/JobTimeLine.jsx
+++ src/components/JobTimeLine.jsx
@@ -1,8 +1,8 @@
 const JobTimeLine = ({ position, description, startedAt, endedAt, company, workScheduleType }) => {
     return (
         <li className="mb-10 ml-4 max-w-[72%] md:max-w-[100%]">
-            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-white dark:ring-[#000000f5] bg-blue-200 dark:bg-gray-400">
-                <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-black" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-white dark:ring-[#09090B] bg-[#18181B] dark:bg-gray-400">
+                <svg aria-hidden="true" className="w-3 h-3 text-[#ffffff] dark:text-[#09090B]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
             </span>
             <time className="text-[.83rem] md:text-[.71rem] font-medium leading-none text-gray-600 dark:text-gray-200">{startedAt} ‑ {endedAt}</time>
             <div className='flex gap-1 items-end'>
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,8 @@
-import '../styles/About.css';
-
 const ProjectCard = ({ name, description, url, skills, color, darkColor }) => {
 
   return (
     <div class="min-w-[42.5%] column dark:opacity-80 dark:hover:opacity-90 opacity-90 hover:opacity-100">
-      <div className={`group card ${darkColor} ${color}`}>
+      <div className={`bg-[#ffffff] dark:bg-[#09090B] border border-[#E4E4E7] dark:border-[#27272A] group card ${darkColor} ${color}`}>
         <div class="txt dark:text-white text-black">
           <h1 className='project-name group-hover:text-white'>{name}</h1>
           <p className='project-desc'>{description}</p>
