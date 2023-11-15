aa3a2888dab1e45f1486ffd7fd902b6301502365
MurAd <muradisayevazer@gmail.com>
2023-11-16 02:02:02 +0400

    Dynamic icons added to Guestbook

--- src/components/NotLoggedInGuest.jsx
+++ src/components/NotLoggedInGuest.jsx
@@ -1,16 +1,29 @@
+import { useEffect, useState } from 'react';
 import { github } from '../assets';
 import { google } from '../assets';
+import { whiteGoogle } from '../assets';
+import { whiteGithub } from '../assets';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+import { isThemeChangedStore } from '../store/useIsThemeChanged';
 
 
 const NotLoggedInGuest = ({ signInWithGitHub, signInWithGoogle }) => {
+  const [getIsDarkTheme] = useLocalStorage("dark-theme");
+  const isThemeChanged = isThemeChangedStore();
+
+  const [isThemeDark, setIsThemeDark] = useState(() => getIsDarkTheme())
+
+  useEffect(() => {
+    setIsThemeDark(getIsDarkTheme())
+  }, [isThemeChanged])
 
   return (
     <div className="flex flex-row items-center gap-6 mb-8 md:gap-4 w-min">
       <div>
         <button className="login-with login-with-github">
-          <div className='w-[21px] h-[21px] object-contain md:w-[14px] md:h-[14px]'>
+          <div className='w-[22px] h-[22px] object-contain md:w-[14px] md:h-[14px]'>
             <img
-              src={github}
+              src={isThemeDark ? github : whiteGithub}
               alt="github logo"
             />
           </div>
@@ -19,9 +32,9 @@ const NotLoggedInGuest = ({ signInWithGitHub, signInWithGoogle }) => {
       </div>
       <div>
         <button className="login-with login-with-google">
-          <div className='w-[17.5px] h-[17.5px] md:w-[12px] md:h-[12px]'>
+          <div className='w-[22px] h-[22px] object-contain md:w-[14px] md:h-[14px]'>
             <img
-              src={google}
+              src={isThemeDark ? whiteGoogle : google}
               alt="google logo"
             />
           </div>
