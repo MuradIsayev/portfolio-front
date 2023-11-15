45df30b182b1b0de76a4f5b9598b8513224f3841
MurAd <muradisayevazer@gmail.com>
2023-11-16 01:59:04 +0400

    exports updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,12 @@
 import goBack from './goback.svg';
-import github from './socials/bluegithub.svg';
+import github from './socials/github.svg';
+import whiteGithub from './socials/whitegithub.svg';
 import google from './socials/google.svg';
 import linkedin from './socials/bluelinkedin.svg';
 import myAvatar from './myavatar.jpg';
 import underConstruction from './underConstruction.json';
 import notFound from './notFound.json';
+import darkLinkedin from './socials/darklinkedin.svg';
+import whiteGoogle from './socials/whitegoogle.svg';
 
-export { goBack, github, google, linkedin, myAvatar, underConstruction, notFound };
+export { goBack, github, google, linkedin, myAvatar, underConstruction, notFound, whiteGithub, darkLinkedin, whiteGoogle };
