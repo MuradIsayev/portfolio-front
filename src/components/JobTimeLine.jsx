c62976e4166db7180f49917f3c1f2be5e4215e36
MurAd <muradisayevazer@gmail.com>
2023-12-18 01:08:29 +0400

    animations for jobs timeline added

--- src/components/JobTimeLine.jsx
+++ src/components/JobTimeLine.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 const JobTimeLine = ({ position, description, startedAt, endedAt, company, workScheduleType }) => {
     return (
-        <motion.li variants={items} className="mb-10 ml-5 max-w-[72%] md:max-w-[100%]">
+        <li className="mb-10 ml-5 max-w-[72%] md:max-w-[100%]">
             <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-[#ffffff] dark:ring-[#09090B] bg-[#18181B] dark:bg-neutral-400">
                 <svg aria-hidden="true" className="w-3 h-3 object-contain text-[#ffffff] dark:text-[#09090B]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
             </span>
@@ -19,7 +19,7 @@ const JobTimeLine = ({ position, description, startedAt, endedAt, company, workS
             <motion.p variants={items} className="mb-4 text-sm font-normal md:text-[.67rem] text-neutral-600 dark:text-neutral-400">
                 {description}
             </motion.p>
-        </motion.li>
+        </li>
     )
 }
 
