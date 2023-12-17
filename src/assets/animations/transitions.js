72ab53ed9608d47b52c2ce1bfda1722463b31d9c
MurAd <muradisayevazer@gmail.com>
2023-12-18 01:08:39 +0400

    transition durations updated

--- src/assets/animations/transitions.js
+++ src/assets/animations/transitions.js
@@ -3,20 +3,20 @@ export const container = {
     show: {
         opacity: 1,
         transition: {
-            staggerChildren: 0.25,
-            delayChildren: .2,
+            staggerChildren: 0.2,
+            delayChildren: .15,
         },
     },
 }
 
-export const items = {
+
+export const cardContainer = {
     hidden: { opacity: 0 },
     show: {
-        opacity: 1, transition: {
-            type: "tween",
-            staggerChildren: 0.25, // 0.25 second delay between each child 
-            delayChildren: .25, // wait .25 before starting 
-            duration: ".7",
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.2,
+            delayChildren: .5   ,
         },
     },
 }
@@ -27,7 +27,19 @@ export const guestbookContainer = {
         opacity: 1,
         transition: {
             staggerChildren: 0.08,
-            delayChildren: .37,
+            delayChildren: .5,
+        },
+    },
+}
+
+export const items = {
+    hidden: { opacity: 0 },
+    show: {
+        opacity: 1, transition: {
+            type: "tween",
+            staggerChildren: 0.2, // 0.25 second delay between each child 
+            delayChildren: .2, // wait .25 before starting 
+            duration: ".6",
         },
     },
 }
