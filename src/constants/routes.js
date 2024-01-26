d952634b8ec55b7e33f02e4ea132ac74a44bf3a3
MuradIsayev <muradisayevazer@gmail.com>
2024-01-26 19:34:00 +0400

    navbar list items separated to the constants folder

---
+++ src/constants/routes.js
@@ -0,0 +1,21 @@
+const navItems = [
+    {
+        path: "/home",
+        name: "Home",
+    },
+    {
+        path: "/about",
+        name: "About",
+    },
+    {
+        path: "/blog",
+        name: "Blog",
+    },
+    {
+        path: "/guestbook",
+        name: "Guestbook",
+    },
+];
+
+
+export default navItems;
