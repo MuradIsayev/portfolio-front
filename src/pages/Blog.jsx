ec5eca2f7819ddf3689748e553d168035e831fa8
MuradIsayev <muradisayevazer@gmail.com>
2024-01-28 02:56:24 +0400

    some redundant code removed

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,33 +1,21 @@
 import { useEffect, useMemo, useState } from 'react';
-import { BlogContent, BlogDetails, BlogTags, CountAnimation } from '../components';
-import { Link } from 'react-router-dom';
+import { BlogContent, CountAnimation } from '../components';
 import '../styles/brackets.scss'
-import { fetchBlogs, fetchRandomPost } from '../api/blog';
+import { fetchBlogs } from '../api/blog';
 import { useQuery } from '@tanstack/react-query';
 
 
 const Blog = () => {
   const [blogData, setBlogData] = useState([]);
-  const [selectedBlogId, setSelectedBlogId] = useState(null);
-  const [selectedBlogTitle, setSelectedBlogTitle] = useState(null);
-  const [selectedBlogCreatedAt, setSelectedBlogCreatedAt] = useState(null);
-  const [selectedBlogMinsRead, setSelectedBlogMinsRead] = useState(null);
 
   const { data: blogs } = useQuery({ queryKey: ['blogs'], queryFn: fetchBlogs })
   // const { data: tags } = useQuery({ queryKey: ['tags'], queryFn: fetchTags }, { staleTime: 3000 })
-  const { data: randomPost } = useQuery({ queryKey: ['randomPost'], queryFn: fetchRandomPost }, { staleTime: 60000 })
+  // const { data: randomPost } = useQuery({ queryKey: ['randomPost'], queryFn: fetchRandomPost }, { staleTime: 60000 })
 
   useEffect(() => {
     if (blogs) setBlogData(blogs)
   }, [blogs])
 
-
-  const handleBlogSelection = (blogId, title, createdAt, minsRead) => {
-    setSelectedBlogId(blogId);
-    setSelectedBlogTitle(title);
-    setSelectedBlogCreatedAt(createdAt);
-    setSelectedBlogMinsRead(minsRead);
-  };
 
   const numberOfBlogs = useMemo(() => {
     return blogData?.length;
@@ -38,45 +26,39 @@ const Blog = () => {
   return (
     <>
       <div
-        className="mt-[7.3rem] w-[63%] md:w-[100%] md:mt-20"
+        className="flex flex-col justify-start w-[90%] mt-[7.3rem] md:mt-20 md:w-auto"
       >
-        {selectedBlogId ? (
-          <BlogDetails blogId={selectedBlogId} setSelectedBlogId={setSelectedBlogId} title={selectedBlogTitle} minsRead={selectedBlogMinsRead} createdAt={selectedBlogCreatedAt} />
-        ) : (
-          <>
-            <div className='flex flex-row justify-between w-[90%] md:w-[95%] items-center'>
-              <h2 className='headers'>Blog</h2>
-              <span className='text-[.92rem] md:text-[.65rem] font-bold'>
-                <CountAnimation n={numberOfBlogs} />
-                <span> articles</span>
-              </span>
-            </div>
-            <div>
-              {blogData?.map(({ id, blockId, title, description, minsRead, createdAt, tags }) => {
-                return (
-                  <BlogContent
-                    key={id}
-                    id={id}
-                    title={title}
-                    blockId={blockId}
-                    description={description}
-                    minsRead={minsRead}
-                    tags={tags}
-                    createdAt={createdAt}
-                    handleClick={handleBlogSelection}
-                    testValue={i += 3}
-                  />
-                );
-              })}
-            </div>
-          </>
-        )}
+        <div className='flex flex-row justify-between w-[65%] md:w-[95%] items-center'>
+          <h2 className='headers'>Blog</h2>
+          <span className='text-[.92rem] md:text-[.65rem] font-bold'>
+            <CountAnimation n={numberOfBlogs} />
+            <span> blogs</span>
+          </span>
+        </div>
+        <div>
+          {blogData?.map(({ id, title, description, minsRead, createdAt, tags, slug }) => {
+            return (
+              <BlogContent
+                key={id}
+                // id={id}
+                slug={slug}
+                title={title}
+                description={description}
+                minsRead={minsRead}
+                tags={tags}
+                createdAt={createdAt}
+                // handleClick={handleBlogSelection}
+                testValue={i += 3}
+              />
+            );
+          })}
+        </div>
       </div>
-      <div className='flex flex-col items-start w-[27%] mt-[7.3rem] pl-1 md:flex-row md:justify-evenly md:w-full'>
+      {/* <div className='flex flex-col items-start w-[27%] mt-[7.3rem] pl-1 md:flex-row md:justify-evenly md:w-full'>
         <div>
           <p className='text-lg headers'>Random Post</p>
           <div>
-            <Link to={`/blogs/${randomPost?.id}`} className='text-[#376ccf] opacity-75 hover:opacity-100 duration-75 ease-linear
+            <Link to={`/blog/${randomPost?.id}`} className='text-[#376ccf] opacity-75 hover:opacity-100 duration-75 ease-linear
            dark:text-[#4673c5]  dark:opacity-80 dark:hover:opacity-100 home-texts brackets brackets2'>{randomPost?.title}</Link>
           </div>
         </div>
@@ -99,7 +81,7 @@ const Blog = () => {
             )}
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
