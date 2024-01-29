71093adb1035eb8d3884a54ea6cb6adb81c828e0
MuradIsayev <muradisayevazer@gmail.com>
2024-01-30 01:48:23 +0400

    Blog content view updated and animation added

--- src/components/BlogContent.jsx
+++ src/components/BlogContent.jsx
@@ -1,43 +1,82 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { items } from "../assets/animations/transitions";
+import BlogTags from "./BlogTags";
+import { useQuery } from "@tanstack/react-query";
+import { updateViewCount } from "../api/blog";
 
-const BlogContent = ({ slug, title, minsRead, createdAt, description, tags, nextDelay }) => {
+
+const BlogContent = ({ slug, title, minsRead, createdAt, description, tags, nextDelay, viewCount }) => {
+
+  const animateTags = {
+    initial: {
+      opacity: 0,
+      x: 15,
+    },
+
+    animateTags: {
+      opacity: 1,
+      x: -10,
+      transition: {
+        type: "spring",
+        stiffness: 210,
+        damping: 15,
+      },
+    },
+  };
 
   return (
     <motion.div
       variants={items}
       initial={{
         opacity: 0,
-      }}
+      }
+      }
       animate={{ opacity: 1 }}
       transition={{ duration: .4, delay: .3 * nextDelay, ease: 'linear' }}
+      onClick={() => updateViewCount(slug)}
     >
       <Link to={`/blog/${slug}`}>
-        <div>
+        <motion.div
+          initial="initial"
+          animate="initial"
+          whileHover="animateTags"
+        >
           <motion.div
             initial={{ y: 0 }}
             whileHover={{ y: -5 }}
             transition={{
               type: "spring",
               stiffness: 210,
-              damping: 30,
+              damping: 15,
             }}
-            className="grow hover:bg-[#ededeee0] dark:hover:bg-[#151516d5] h-[5.2rem] md:h-[4.5rem] flex flex-row flex-wrap cursor-pointer hover:shadow-md border border-[#E4E4E7] dark:border-[#27272A] rounded-lg mb-4 md:mb-3 mt-1.5 card-hover">
-            <div className='flex flex-row flex-wrap justify-center items-center md:justify-start max-w-[100%] md:max-w-[95%] p-4 md:ml-1'>
-              <div className='flex flex-col items-start md:items-start'>
+            className="grow hover:bg-[#ededeee0] dark:hover:bg-[#151516d5] p-4 h-[4.7rem] md:h-[4.5rem] justify-around items-start flex flex-col flex-wrap cursor-pointer hover:shadow-md border border-[#E4E4E7] dark:border-[#27272A] rounded-lg mb-3 md:mb-3 mt-1.5 card-hover">
+            <div className='flex flex-row flex-wrap items-center justify-start md:justify-start md:ml-1'>
+              <div className='flex flex-col items-start justify-start'>
                 <h3 className='text-[1rem] md:text-[.73rem] font-medium'>{title}</h3>
                 <div className='flex gap-1 md:gap-[3px] text-[.75rem] md:text-[.53rem] text-neutral-500'>
-                  <span>{createdAt}</span>
-                  <span>|</span>
-                  <span>{minsRead} min read</span>
+                  {tags?.map(({ id, name }) => {
+                    return (
+                      <BlogTags
+                        key={id}
+                        tag={name}
+                      />
+                    );
+                  })}
+
                 </div>
               </div>
             </div>
+            <motion.div
+              variants={animateTags}
+              className='flex flex-col flex-wrap items-center self-end justify-center text-[.75rem] md:text-[.53rem] text-neutral-500'>
+              <span>{createdAt}</span>
+              <span>{viewCount} views</span>
+            </motion.div>
           </motion.div>
-        </div>
+        </motion.div>
       </Link>
-    </motion.div>
+    </motion.div >
   );
 }
 
