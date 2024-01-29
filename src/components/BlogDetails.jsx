8399ec1a82bcd73e0cece64fd13184749e333d97
MuradIsayev <muradisayevazer@gmail.com>
2024-01-29 20:15:03 +0400

    improvements on blog content and details components

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,13 +1,29 @@
-import { goBack } from '../assets';
+import { goBack, darkGoBack } from '../assets';
 import { useNavigate, useParams } from 'react-router-dom';
 import { fetchBlogById } from '../api/blog';
 import { useQuery } from '@tanstack/react-query';
 import ReactMarkdown from 'react-markdown';
+import { useEffect, useState } from 'react';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+import { isThemeChangedStore } from '../store/useIsThemeChanged';
+import { motion } from 'framer-motion';
+
+
 
 
 const BlogDetails = () => {
     let { slug } = useParams();
     const navigate = useNavigate();
+
+    const [getIsDarkTheme] = useLocalStorage("dark-theme");
+    const isThemeChanged = isThemeChangedStore();
+
+    const [isThemeDark, setIsThemeDark] = useState(() => getIsDarkTheme())
+
+    useEffect(() => {
+        setIsThemeDark(getIsDarkTheme())
+    }, [isThemeChanged])
+
 
     const handleGoBack = () => {
         navigate('/blog');
@@ -19,20 +35,29 @@ const BlogDetails = () => {
 
     return (
         <div className="flex flex-col justify-start w-[90%] mt-[7.3rem] md:mt-20 md:w-full">
-            <div className='flex flex-row items-center w-[65%] mb-1 gap-3'
+            <div className='flex flex-row items-center w-[65%] gap-3'
             >
-                <div onClick={handleGoBack} className='flex flex-row gap-[6px] cursor-pointer'>
-                    <img src={goBack} alt='Go back icon' className='w-6' />
-                </div>
-                <div className=''>
-                    <h3 className='text-[1.6rem] md:text-[1.32rem] font-medium'>{singleBlog?.post?.title}</h3>
+                <motion.div
+                    initial={{ x: 0 }}
+                    animate={{ x: -4 }}
+                    transition={{
+                        type: "tween",
+                        repeat: Infinity,
+                        repeatType: "reverse",
+                        duration: 0.6,
+                        ease: 'easeInOut',
+                        delay: 2
+                    }}
+                    onClick={handleGoBack} className='transition duration-150 ease-linear cursor-pointer opacity-70 hover:opacity-100'>
+                    <img src={isThemeDark ? goBack : darkGoBack} alt='Go back icon' className='w-7 md:w-5' />
+                </motion.div>
+                <div>
+                    <h3 className='text-[1.65rem] font-semibold md:text-[1.32rem]'>{singleBlog?.post?.title}</h3>
                 </div>
             </div>
             <div className=' mb-4 flex flex-row items-center justify-between w-[65%]'>
                 <div className='flex gap-1 md:gap-[4px] text-[.8rem] md:text-[.58rem] text-neutral-600 dark:text-neutral-400'>
-                    <span>{singleBlog?.post?.createdAt}</span>
-                    <span>|</span>
-                    <span>{singleBlog?.post?.minsRead} min read</span>
+                    <span>{singleBlog?.post?.createdAt} | {singleBlog?.post?.minsRead} min read</span>
                 </div>
                 <div className='text-[.8rem] md:text-[.58rem] text-neutral-600 dark:text-neutral-400'>
                     123 Views
