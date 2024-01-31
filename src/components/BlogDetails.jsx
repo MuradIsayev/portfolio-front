e5e3f99dbb3e4b32f435c83ec05f8c7d6243a131
MuradIsayev <muradisayevazer@gmail.com>
2024-01-31 14:49:14 +0400

    Blog skeleton for blog details page added

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from 'react';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 import { isThemeChangedStore } from '../store/useIsThemeChanged';
 import { motion } from 'framer-motion';
-
+import { BlogDetailsSkeleton } from '../components';
 
 const BlogDetails = () => {
     let { slug } = useParams();
@@ -26,41 +26,45 @@ const BlogDetails = () => {
         navigate('/blog');
     };
 
-    const { data: singleBlog } = useQuery({ queryKey: ['singleBlog', slug], queryFn: () => fetchBlogById(slug) });
+    const { data: singleBlog, isLoading } = useQuery({ queryKey: ['singleBlog', slug], queryFn: () => fetchBlogById(slug) });
 
     return (
         <div className="flex flex-col justify-start w-[90%] mt-[7.3rem] md:mt-20 md:w-full">
-            <div className='flex flex-row items-center w-[65%] gap-3 mb-1'
-            >
-                <motion.div
-                    initial={{ x: 0 }}
-                    animate={{ x: -4 }}
-                    transition={{
-                        type: "tween",
-                        repeat: Infinity,
-                        repeatType: "reverse",
-                        duration: 0.6,
-                        ease: 'easeInOut',
-                        delay: 2
-                    }}
-                    onClick={handleGoBack} className='transition duration-150 ease-linear cursor-pointer opacity-70 hover:opacity-100'>
-                    <img src={isThemeDark ? goBack : darkGoBack} alt='Go back icon' className='w-7 md:w-5' />
-                </motion.div>
-                <div>
-                    <div className='text-[1.45rem] font-semibold md:text-[1.32rem] leading-6'>{singleBlog?.post?.title}</div>
-                </div>
-            </div>
-            <div className=' mb-4 flex flex-row items-center justify-between w-[65%]'>
-                <div className='flex gap-1 md:gap-[4px] text-[.8rem] md:text-[.58rem] text-neutral-600 dark:text-neutral-400 font-medium'>
-                    {singleBlog?.post?.minsRead} min read
-                </div>
-                <div className='text-[.8rem] md:text-[.58rem] text-neutral-600 dark:text-neutral-400 font-medium'>
-                    {singleBlog?.post?.fromNow}
-                </div>
-            </div>
-            <article className='prose mb-5 md:mb-3 md:prose-sm !prose-slate dark:!prose-invert dark:prose-pre:bg-[#151516d5] dark:prose-pre:text-neutral-400 prose-pre:bg-[#ededeee0] prose-pre:text-neutral-900 text-black dark:text-white lg:max-w-[65%] md:max-w-full mt-5 md:mt-3'>
-                <ReactMarkdown>{singleBlog?.markdown?.parent}</ReactMarkdown>
-            </article>
+            {isLoading ? <BlogDetailsSkeleton /> : (
+                <>
+                    <div className='flex flex-row items-center w-[65%] md:w-full gap-3 mb-1'
+                    >
+                        <motion.div
+                            initial={{ x: 0 }}
+                            animate={{ x: -4 }}
+                            transition={{
+                                type: "tween",
+                                repeat: Infinity,
+                                repeatType: "reverse",
+                                duration: 0.6,
+                                ease: 'easeInOut',
+                                delay: 2
+                            }}
+                            onClick={handleGoBack} className='transition duration-150 ease-linear cursor-pointer opacity-70 hover:opacity-100'>
+                            <img src={isThemeDark ? goBack : darkGoBack} alt='Go back icon' className='w-7' />
+                        </motion.div>
+                        <div>
+                            <div className='text-[1.45rem] font-semibold md:text-[1.15rem] leading-6 md:leading-5'>{singleBlog?.post?.title}</div>
+                        </div>
+                    </div>
+                    <div className=' mb-4 flex flex-row items-center justify-between w-[65%] md:w-full'>
+                        <div className='flex gap-1 md:gap-[4px] text-[.8rem] md:text-[.55rem] text-neutral-600 dark:text-neutral-400 font-medium'>
+                            {singleBlog?.post?.minsRead} min read
+                        </div>
+                        <div className='text-[.8rem] md:text-[.55rem] text-neutral-600 dark:text-neutral-400 font-medium'>
+                            {singleBlog?.post?.fromNow}
+                        </div>
+                    </div>
+                    <article className='prose mb-5 md:mb-3 md:prose-sm !prose-slate dark:!prose-invert dark:prose-pre:bg-[#151516d5] dark:prose-pre:text-neutral-400 prose-pre:bg-[#ededeee0] prose-pre:text-neutral-900 text-black dark:text-white lg:max-w-[65%] md:max-w-full mt-5 md:mt-3'>
+                        <ReactMarkdown>{singleBlog?.markdown?.parent}</ReactMarkdown>
+                    </article>
+                </>
+            )}
         </div>
     );
 }
---
+++ src/components/BlogDetailsSkeleton.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const BlogDetailsSkeleton = () => {
+    return (
+        <>
+            <div className='flex flex-col items-center w-[65%] md:w-full gap-3 md:gap-2'>
+                <div className='w-full md:[50%] h-9 md:h-7 animate-pulse bg-neutral-300 rounded dark:bg-neutral-800 '>
+                </div>
+                <div className='flex flex-row items-center justify-between w-full mb-4 md:mb-3'>
+                    <div className='flex h-3 gap-1 rounded md:w-12 w-14 animate-pulse bg-neutral-300 dark:bg-neutral-800 '>
+                    </div>
+                    <div className='h-3 rounded w-14 md:w-12 animate-pulse bg-neutral-300 dark:bg-neutral-800'>
+                    </div>
+                </div>
+                {Array(5).fill().map((_, i) => (
+                    <div key={i} className='w-full mt-3'>
+                        <div className='w-[70%] h-8 md:h-6 animate-pulse bg-neutral-300 rounded dark:bg-neutral-800 mb-3'>
+                        </div>
+                        <article className='w-full h-56 mb-5 rounded md:h-44 md:h-mt-5 md:mt-3 md:mb-3 animate-pulse bg-neutral-300 dark:bg-neutral-800'>
+                        </article>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default BlogDetailsSkeleton
