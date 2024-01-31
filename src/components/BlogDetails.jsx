abfee456ed5eedaa4d2efabf63ea31415907de77
MuradIsayev <muradisayevazer@gmail.com>
2024-01-31 19:50:23 +0400

    go back button ui changed and bug fixed

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -29,38 +29,41 @@ const BlogDetails = () => {
     const { data: singleBlog, isLoading } = useQuery({ queryKey: ['singleBlog', slug], queryFn: () => fetchBlogById(slug) });
 
     return (
-        <div className="flex flex-col justify-start w-[90%] mt-[7.3rem] md:mt-20 md:w-full">
+
+        <div className="flex flex-col justify-start w-[90%] mt-[7.3rem] md:mt-24 md:w-full mb-10 md:mb-5 ">
+            <motion.div
+                initial={{ x: 0 }}
+                animate={{ x: -4 }}
+                transition={{
+                    type: "tween",
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                    duration: 0.6,
+                    ease: 'easeInOut',
+                }}
+                onClick={handleGoBack} className='
+                flex md:h-7 md:w-7 h-9 w-9 p-1
+                dark:hover:bg-[#151516d5] rounded-md hover:bg-[#ededeee0]
+                absolute -mt-10 transition duration-150 ease-linear cursor-pointer md:-mt-7'>
+                <img src={isThemeDark ? goBack : darkGoBack} alt='Go back icon' />
+            </motion.div>
+
             {isLoading ? <BlogDetailsSkeleton /> : (
                 <>
-                    <div className='flex flex-row items-center w-[65%] md:w-full gap-3 mb-1'
-                    >
-                        <motion.div
-                            initial={{ x: 0 }}
-                            animate={{ x: -4 }}
-                            transition={{
-                                type: "tween",
-                                repeat: Infinity,
-                                repeatType: "reverse",
-                                duration: 0.6,
-                                ease: 'easeInOut',
-                                delay: 2
-                            }}
-                            onClick={handleGoBack} className='transition duration-150 ease-linear cursor-pointer opacity-70 hover:opacity-100'>
-                            <img src={isThemeDark ? goBack : darkGoBack} alt='Go back icon' className='w-7' />
-                        </motion.div>
-                        <div>
-                            <div className='text-[1.45rem] font-semibold md:text-[1.15rem] leading-6 md:leading-5'>{singleBlog?.post?.title}</div>
-                        </div>
+
+                    <div className='flex flex-row items-center w-[65%] md:w-full md:gap-2 gap-3 mb-2 md:mb-1'>
+
+                        <div className='text-[1.45rem] font-medium md:text-[1.15rem] leading-6 md:leading-5 '>{singleBlog?.post?.title}</div>
                     </div>
-                    <div className=' mb-4 flex flex-row items-center justify-between w-[65%] md:w-full'>
-                        <div className='flex gap-1 md:gap-[4px] text-[.8rem] md:text-[.55rem] text-neutral-600 dark:text-neutral-400 font-medium'>
+                    <div className=' mb-4 flex flex-row items-center justify-between w-[65%] md:w-full  '>
+                        <div className='flex gap-1 md:gap-[4px] text-[.85rem] md:text-[.58rem] text-neutral-600 dark:text-neutral-400 font-medium'>
                             {singleBlog?.post?.minsRead} min read
                         </div>
-                        <div className='text-[.8rem] md:text-[.55rem] text-neutral-600 dark:text-neutral-400 font-medium'>
+                        <div className='text-[.85rem] md:text-[.58rem] text-neutral-600 dark:text-neutral-400 font-medium'>
                             {singleBlog?.post?.fromNow}
                         </div>
                     </div>
-                    <article className='prose mb-5 md:mb-3 md:prose-sm !prose-slate dark:!prose-invert dark:prose-pre:bg-[#151516d5] dark:prose-pre:text-neutral-400 prose-pre:bg-[#ededeee0] prose-pre:text-neutral-900 text-black dark:text-white lg:max-w-[65%] md:max-w-full mt-5 md:mt-3'>
+                    <article className='prose md:prose-sm !prose-neutral dark:!prose-invert dark:prose-pre:bg-[#151516d5] dark:prose-pre:text-neutral-300 prose-pre:bg-[#ededeee0] prose-pre:text-neutral-900 min-w-[65%] md:w-full mt-5 md:mt-4'>
                         <ReactMarkdown>{singleBlog?.markdown?.parent}</ReactMarkdown>
                     </article>
                 </>
