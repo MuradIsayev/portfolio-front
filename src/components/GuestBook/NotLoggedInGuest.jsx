d63ed1f43025fd588795001d46596c851e445937
MuradIsayev <muradisayevazer@gmail.com>
2024-02-01 13:09:07 +0400

    responsiveness handling improved on guestbook buttons

--- src/components/GuestBook/NotLoggedInGuest.jsx
+++ src/components/GuestBook/NotLoggedInGuest.jsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from 'react';
-import { github } from '../../assets';
-import { google } from '../../assets';
-import { whiteGoogle } from '../../assets';
-import { whiteGithub } from '../../assets';
+import { github, google, whiteGoogle, whiteGithub } from '../../assets';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { isThemeChangedStore } from '../../store/useIsThemeChanged';
 
@@ -19,29 +16,25 @@ const NotLoggedInGuest = ({ signInWithGitHub, signInWithGoogle }) => {
   }, [isThemeChanged])
 
   return (
-    <div className="flex flex-row items-center gap-6 mb-8 md:gap-4 w-min">
-      <div>
-        <button className="login-with login-with-github">
-          <div className='w-[22px] h-[22px] object-contain md:w-[14px] md:h-[14px] '>
-            <img
-              src={isThemeDark ? github : whiteGithub}
-              alt="github logo"
-            />
-          </div>
-          <span className="text-[.83rem] font-[500] md:text-[.5rem]" onClick={signInWithGitHub}>Sign in with GitHub</span>
-        </button>
-      </div>
-      <div>
-        <button className="login-with login-with-google">
-          <div className='w-[22px] h-[22px] object-contain md:w-[14px] md:h-[14px]'>
-            <img
-              src={isThemeDark ? whiteGoogle : google}
-              alt="google logo"
-            />
-          </div>
-          <span className="text-[.83rem] font-[500] md:text-[.5rem]" onClick={signInWithGoogle}>Sign in with Google</span>
-        </button>
-      </div>
+    <div className="flex flex-row items-center gap-5 mb-8 md:gap-4 lg:w-[70%] w-[80%] md:w-full">
+      <button className="login-with login-with-github">
+        <div className='w-[22px] h-[22px] object-contain md:w-[14px] md:h-[14px]'>
+          <img
+            src={isThemeDark ? github : whiteGithub}
+            alt="github logo"
+          />
+        </div>
+        <span className="lg:text-[.83rem] text-[.77rem] font-[500] md:text-[.5rem]" onClick={signInWithGitHub}>Sign in with GitHub</span>
+      </button>
+      <button className="login-with login-with-google">
+        <div className='w-[22px] h-[22px] object-contain md:w-[14px] md:h-[14px]'>
+          <img
+            src={isThemeDark ? whiteGoogle : google}
+            alt="google logo"
+          />
+        </div>
+        <span className="lg:text-[.83rem] text-[.77rem] font-[500] md:text-[.5rem]" onClick={signInWithGoogle}>Sign in with Google</span>
+      </button>
     </div>
   );
 }
