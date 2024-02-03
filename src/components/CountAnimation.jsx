5a46e6a7abf349139392a9cc9b5b0275f1126bea
MuradIsayev <muradisayevazer@gmail.com>
2024-02-03 21:08:14 +0400

    custom duration added for count animation

--- src/components/CountAnimation.jsx
+++ src/components/CountAnimation.jsx
@@ -5,9 +5,28 @@ const CountAnimation = ({ n }) => {
     const count = useMotionValue(0);
     const rounded = useTransform(count, Math.round);
 
+    let customDuration;
+
+    switch (true) {
+        case n > 5:
+            customDuration = .5;
+            break;
+        case n > 10:
+            customDuration = .7;
+            break;
+        case n > 20:
+            customDuration = .9;
+            break;
+        case n > 30:
+            customDuration = 1;
+            break;
+        default:
+            customDuration = 0.3;
+    }
+
     useEffect(() => {
         const animation = animate(count, n, {
-            duration: n > 10 ? 1 : 0.5,
+            duration: customDuration
         });
 
         return animation.stop;
