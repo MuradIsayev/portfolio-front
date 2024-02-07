f2a13214cf8cef71acb115ea81a99a5f7b16e09a
MuradIsayev <muradisayevazer@gmail.com>
2024-02-07 18:44:02 +0400

    tool-tip added

--- src/components/GuestBook/GuestBookContent.jsx
+++ src/components/GuestBook/GuestBookContent.jsx
@@ -1,4 +1,18 @@
+import { Tooltip } from 'react-tooltip'
+import { isThemeChangedStore } from '../../store/useIsThemeChanged';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { useEffect, useState } from 'react';
+
 const GuestBookContent = ({ data }) => {
+
+  const [getIsDarkTheme] = useLocalStorage("dark-theme");
+  const isThemeChanged = isThemeChangedStore();
+  const [isThemeDark, setIsThemeDark] = useState(() => getIsDarkTheme())
+
+  useEffect(() => {
+    setIsThemeDark(getIsDarkTheme())
+  }, [isThemeChanged])
+
 
   return (
     <div
@@ -6,9 +20,14 @@ const GuestBookContent = ({ data }) => {
       {data?.map((message, index) => (
         <div key={index} className="mt-2">
           <div key={message?.createdAt} className="flex flex-row items-center guestbook-container">
+            <div className="w-10 h-10 lg:w-11 lg:h-11 rounded-full object-contain md:w-7 md:h-7 mr-[7px] md:mr-[3px] relative mb-auto"
+            >
 
-            <div className="w-10 h-10 lg:w-11 lg:h-11 rounded-full object-contain md:w-7 md:h-7 mr-[7px] md:mr-[3px] relative mb-auto tooltip" data-tip={message?.createdAt}>
               <img
+                data-tooltip-id="my-tooltip"
+                data-tooltip-content={message?.createdAt}
+                data-tooltip-place="top"
+                data-tooltip-variant={isThemeDark ? "light" : "dark"}
                 src={message?.photoURL} alt="Profile photo" className="rounded-full " />
               {message?.isOnline ? <span className="top-0 left-7 absolute w-3 h-3 bg-green-400 border-2 border-white dark:border-[#09090B] rounded-full
                   md:left-5 md:w-[7px] md:h-[7px] md:border-[1px]
@@ -30,6 +49,13 @@ const GuestBookContent = ({ data }) => {
         </div>
       ))
       }
+      <Tooltip id="my-tooltip" opacity={1}
+        style={{
+          borderRadius: "6px",
+          padding: "6px 9px",
+          fontSize: "1.6vmin",
+        }}
+      />
     </div >
 
   );
