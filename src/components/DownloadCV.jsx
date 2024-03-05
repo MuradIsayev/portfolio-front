7cd657590f26ac10a4574279c43a2b5ade3e0c84
MuradIsayev <muradisayevazer@gmail.com>
2024-03-05 21:09:15 +0400

    final changes to the download cv button

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -53,7 +53,7 @@ const DownloadCV = () => {
     return (
         <div className="grid mt-3 rounded-md download-button">
             <div className="wrapper rounded-md h-[3.5rem] w-[10rem] md:h-[2.5rem]
-                             md:w-[5.8rem] overflow-hidden cursor-pointer relative" onClick={() => !isAnimating && animate()}>
+                             md:w-[6.7rem] overflow-hidden cursor-pointer relative" onClick={() => !isAnimating && animate()}>
                 <AnimatePresence>
                     <motion.div
                         className="home-download-buttons md:text-[.75rem] top-0 z-[1] transition ease-linear duration-150
@@ -73,7 +73,7 @@ const DownloadCV = () => {
                                 ease: 'easeInOut',
                                 delay: 2
                             }}><FaAngleDoubleDown className="md:w-[9px] md:h-[9px] h-[14px] w-[14px] icon dark:text-neutral-400 text-neutral-600" /></motion.div>
-                        <div className='dark:text-neutral-400 text-neutral-600 text-[.97rem] md:text-xs'>CV</div>
+                        <div className='dark:text-neutral-400 text-neutral-600 text-[.97rem] md:text-[.67rem]'>CV</div>
                     </motion.div>
                     <motion.div className="home-download-buttons md:text-[.7rem] z-[2] top-[-100%] 
                                          bg-neutral-600 dark:bg-neutral-500"
@@ -84,7 +84,7 @@ const DownloadCV = () => {
                                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                             </svg>
                         </div>
-                        <div className='text-[#fafafa] dark:text-[#09090B]'>Loading...</div>
+                        <div className='text-[#fafafa] dark:text-[#09090B] text-[.97rem] md:text-[.67rem]'>Loading...</div>
                         <motion.div className="absolute left-0 top-[calc(100%-4px)] md:top-[calc(100%-3px)] dark:bg-[#09090bdc] bg-[#f7f7f7e8] h-1 md:h-[3px] w-[0%] bottom-0" animate={loadingBarControls}>
                         </motion.div>
                     </motion.div>
@@ -92,7 +92,7 @@ const DownloadCV = () => {
                     dark:bg-neutral-400 dark:text-black bg-neutral-700 text-white"
                         animate={doneControls}>
                         <FaCheck className="icon" />
-                        <div>DONE</div>
+                        <div className='text-[.97rem] md:text-[.67rem]'>DONE</div>
                     </motion.div>
                 </AnimatePresence>
             </div>
