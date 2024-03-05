1a28ba7e4fc74238941d5d03909e8b62039cd825
MuradIsayev <muradisayevazer@gmail.com>
2024-03-05 20:53:05 +0400

    animation ui and sizing modified

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -14,7 +14,7 @@ const DownloadCV = () => {
 
         setIsAnimating(true);
         uploadControls.start({ zIndex: 1 });
-        await loadingControls.start({ top: 0, transition: { duration: 0.4 } });
+        await loadingControls.start({ top: 0, transition: { duration: .4 } });
         loadingBarControls.start({ width: '100%', transition: { duration: 1.1 } });
 
         try {
@@ -52,12 +52,14 @@ const DownloadCV = () => {
 
     return (
         <div className="grid mt-3 rounded-md download-button">
-            <div className="wrapper rounded-md h-[3.5rem] w-[9rem] md:h-[2.5rem]
+            <div className="wrapper rounded-md h-[3.5rem] w-[10rem] md:h-[2.5rem]
                              md:w-[5.8rem] overflow-hidden cursor-pointer relative" onClick={() => !isAnimating && animate()}>
                 <AnimatePresence>
                     <motion.div
                         className="home-download-buttons md:text-[.75rem] top-0 z-[1] transition ease-linear duration-150
-                        bg-[#18181B] hover:bg-neutral-800 dark:bg-[#F1F1F1] dark:hover:bg-[#f7f7f7e8]"
+                        bg-[#ffffff] text-[#09090B] dark:text-[#fafafa] dark:bg-[#09090B] hover:bg-[#ededeee0] dark:hover:bg-[#151516d5]
+                         border border-[#E4E4E7] dark:border-[#27272A]
+                        "
                         animate={uploadControls}
                     >
                         <motion.div
@@ -70,14 +72,14 @@ const DownloadCV = () => {
                                 duration: 0.45,
                                 ease: 'easeInOut',
                                 delay: 2
-                            }}><FaAngleDoubleDown className="md:w-[9px] md:h-[9px] h-[14px] w-[14px] icon dark:text-[#18181B] text-[#FAFAFA]" /></motion.div>
-                        <div className='dark:text-[#18181B] text-[#FAFAFA] text-[.97rem] md:text-xs'>CV</div>
+                            }}><FaAngleDoubleDown className="md:w-[9px] md:h-[9px] h-[14px] w-[14px] icon dark:text-neutral-400 text-neutral-600" /></motion.div>
+                        <div className='dark:text-neutral-400 text-neutral-600 text-[.97rem] md:text-xs'>CV</div>
                     </motion.div>
                     <motion.div className="home-download-buttons md:text-[.7rem] z-[2] top-[-100%] 
-                                         dark:bg-[#d1d1d1] bg-neutral-800"
+                                         bg-neutral-600 dark:bg-neutral-500"
                         animate={loadingControls}>
                         <div role="status">
-                            <svg aria-hidden="true" className="inline w-[1.2rem] h-[1.2rem] md:w-[.9rem] md:h-[.9rem] text-neutral-800 dark:text-[#d1d1d1] fill-[#fafafa] animate-spin dark:fill-[#09090B]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg aria-hidden="true" className="inline w-[1.2rem] h-[1.2rem] md:w-[.9rem] md:h-[.9rem] text-neutral-600 dark:text-neutral-500 fill-[#fafafa] animate-spin dark:fill-[#09090B]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
                                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                             </svg>
