17b5f655fc994a5c99c73d856366afbc69e02013
MuradIsayev <muradisayevazer@gmail.com>
2024-03-05 11:08:23 +0400

    home page modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { myAvatar, github, linkedin, darkLinkedin, whiteGithub } from '../assets';
+import { avatar, github, linkedin, darkLinkedin, whiteGithub } from '../assets';
 import { motion } from 'framer-motion';
 import { DownloadCV } from '../components';
 import { useThemeStore } from '../store/useThemeDark';
@@ -20,17 +20,18 @@ const Home = () => {
       </motion.p>
 
       <div className="flex flex-row items-center justify-start gap-8 mx-auto my-5 md:gap-5 md:mx-auto md:my-4">
-        <motion.div variants={items}><img className="transition duration-200 ease-linear avatar-img dark:opacity-90 dark:hover:opacity-100 dark:grayscale"
-          src={myAvatar} alt="portfolio avatar" />
+        <motion.div variants={items}>
+          <img className='avatar-img'
+            src={avatar} alt="portfolio avatar" />
         </motion.div>
-        <motion.div variants={items} className=" flex flex-col gap-3 justify-center md:items-start md:gap-[10px]">
+        <motion.div variants={items} className=" flex flex-col justify-center md:items-start gap-[10px]">
           <a className="home-links group" href="https://github.com/MuradIsayev" target='_blank' >
             <img
-              className="group-hover:-translate-y-1 transition-all duration-300 h-[28px] lg:h-[30px] md:h-[21px]"
+              className="group-hover:-translate-y-1 transition-all duration-300 h-[30px] lg:h-[32px] md:h-[23px]"
               src={isThemeDark ? whiteGithub : github}
               alt="github logo"
             />
-            <p className="md:text-[.65rem] lg:text-[.9rem]">My Github</p>
+            <p className="md:text-[.65rem] lg:text-[.9rem] home-texts font-medium text-neutral-600 dark:text-neutral-400">My Github</p>
           </a>
           <a
             target='_blank'
@@ -38,14 +39,14 @@ const Home = () => {
             href="https://www.linkedin.com/in/mi03/"
           >
             <img
-              className="h-[28px] md:h-[21px] lg:h-[30px] group-hover:-translate-y-1 transition-all duration-300"
+              className="h-[30px] md:h-[23px] lg:h-[32px] group-hover:-translate-y-1 transition-all duration-300"
               src={isThemeDark ? linkedin : darkLinkedin}
               alt="linkedin logo"
             />
-            <p className="md:text-[.65rem] lg:text-[.9rem]">My LinkedIn</p>
+            <p className="md:text-[.65rem] lg:text-[.9rem] home-texts font-medium text-neutral-600 dark:text-neutral-400">My LinkedIn</p>
           </a>
         </motion.div>
-      </div>
+      </div >
       <motion.p variants={items} className="home-texts">
         I am a computer science senior at French-Azerbaijani University (UFAZ) and experienced in NodeJS.
         Download my CV to learn more.
@@ -54,7 +55,7 @@ const Home = () => {
       <motion.div variants={items}>
         <DownloadCV />
       </motion.div>
-    </motion.div>
+    </motion.div >
   );
 }
 
