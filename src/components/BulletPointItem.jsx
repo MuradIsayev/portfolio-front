3261b2268597e8d6672003e836727a07e344517d
MuradIsayev <muradisayevazer@gmail.com>
2024-03-06 19:11:41 +0400

    delay animation added for now page

--- src/components/BulletPointItem.jsx
+++ src/components/BulletPointItem.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { dot, darkDot } from '../assets'
 import { useThemeStore } from '../store/useThemeDark';
+import { motion } from 'framer-motion';
+import { items } from '../assets/animations/transitions';
 
 const BulletPointItem = ({ text }) => {
     const { isThemeDark } = useThemeStore();
 
     return (
         <>
-            <div className='flex items-center gap-[6px]'>
+            <motion.div variants={items} className='flex items-center gap-[6px]'>
                 <img src={isThemeDark ? dot : darkDot} className='h-[9px]' alt="bullet point" />
                 <span >{text}</span>
-            </div>
+            </motion.div>
         </>
     )
 }
--- src/pages/Now.jsx
+++ src/pages/Now.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import { BulletPointItem } from '../components'
+import { motion } from 'framer-motion'
+import { container, items } from '../assets/animations/transitions'
 
 
 const Now = () => {
 
     return (
-        <div className='lg:max-w-[60%] max-w-[70%] md:max-w-full'>
-            <h2 className='headers'>What I am up to now</h2>
+        <motion.div
+            variants={container}
+            initial="hidden"
+            animate="show"
+            className='lg:max-w-[60%] max-w-[70%] md:max-w-full'>
+            <motion.h2 variants={items} className='headers'>What I am up to now</motion.h2>
             <div className='home-texts'>
-                <div>This section is a <a
-                    target='_blank'
-                    className="custom-underline" href='https://nownownow.com/about'>/now page</a>, <span>inspired by </span>
+                <motion.div variants={items}>
+                    This section is a <a
+                        target='_blank'
+                        className="custom-underline" href='https://nownownow.com/about'>/now page</a>, <span>inspired by </span>
                     <a className="custom-underline" href="https://sive.rs/" target='_blank'
-                    >Derek Sivers</a>. Here you will find whatever I am currently building, reading and learning.</div>
+                    >Derek Sivers</a>. Here you will find whatever I am currently building, reading and learning.</motion.div>
 
-                <div className='mt-5 mb-2 text-xl md:text-base headers'>Top of my mind at the moment:</div>
+                <motion.div
+                    variants={items}
+                    className='mt-5 mb-2 text-xl md:text-base headers'>Top of my mind at the moment:</motion.div>
 
                 <div className='flex flex-col gap-2 mt-2 ml-5'>
                     <BulletPointItem text='Building a personal website' />
@@ -25,11 +34,11 @@ const Now = () => {
                 </div>
             </div>
 
-            <hr class="h-[1.3px] mb-2 mt-4 bg-[#ededeee0] border-0 dark:bg-[#1f1f20d5]"></hr>
-            <div className='home-texts'>
+            <motion.hr variants={items} class="h-[1.3px] mb-2 mt-4 bg-[#ededeee0] border-0 dark:bg-[#1f1f20d5]"></motion.hr>
+            <motion.div variants={items} className='home-texts'>
                 Last updated on 25 February, 2024 (Baku, Azerbaijan)
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     )
 }
 
